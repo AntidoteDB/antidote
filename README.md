17ed1f622db2ca6ada6acc286eaaa692c766c17c
angbrav <manuelbravogestoso@gmail.com>
2014-03-17 14:04:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+floppy: A Riak Core Application
+======================================
+
+Application Structure
+---------------------
+
+This is a blank riak core application. To get started, you'll want to edit the
+following files:
+
+* `src/riak_floppy_vnode.erl`
+  * Implementation of the riak_core_vnode behaviour
+* `src/floppy.erl`
+  * Public API for interacting with your vnode
---
+++ src/floppy_app.erl
@@ -0,0 +1,27 @@
+-module(floppy_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    case floppy_sup:start_link() of
+        {ok, Pid} ->
+            ok = riak_core:register([{vnode_module, floppy_vnode}]),
+
+            ok = riak_core_ring_events:add_guarded_handler(floppy_ring_event_handler, []),
+            ok = riak_core_node_watcher_events:add_guarded_handler(floppy_node_event_handler, []),
+            ok = riak_core_node_watcher:service_up(floppy, self()),
+
+            {ok, Pid};
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+stop(_State) ->
+    ok.
