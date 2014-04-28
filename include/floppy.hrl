a317e6e883114fc9844539191458f01be71bdb92
Alejandro Z. Tomsic <aletomsic@gmail.com>
2014-04-28 11:24:18 +0200

    updated clockSI_tx_coord

--- include/floppy.hrl
+++ include/floppy.hrl
@@ -11,3 +11,17 @@
 -define(NUM_R, 2).
 -define(OTHER_DC, 'floppy1@127.0.0.1').
 -record(operation, {op_number, payload}).
+
+
+% Clock SI
+
+% MIN is Used for generating the timeStamp of a new snapshot
+% in the case that a client has already seen a snapshot time
+% greater than the current time at the replica it is starting
+% a new transaction.
+
+% DELTA has the same meaning as in the clock-SI paper.
+
+-define(MIN, 1).
+-define(DELTA, 10000). 
+-record(tx, {snapshot_time, commit_time, prepare_time, state, write_set, operations}).
--- src/clockSI_tx_coord_fsm.erl
+++ src/clockSI_tx_coord_fsm.erl
@@ -13,7 +13,7 @@
          handle_sync_event/4, terminate/3]).
 
 %% States
--export([prepare/2, execute/2, finishOp/3]).
+-export([prepare/2, execute/2, finishOp/3, prepare_2PC/2, receive_prepared/2, committing/2, receive_committed/2]).
 
 
 %-record(operationCSI, {opType, key, params}).
@@ -21,18 +21,19 @@
 
 -record(state, {
                 from :: pid(),
+                transaction :: #tx{},
                 operations :: list,
-                snapshotTime = undefined :: term() | undefined,
-                txType = undefined :: term() | undefined,
+                updated_partitions :: list, 
                 currentOp = undefined :: term() | undefined,
+                num_to_ack :: int, 
                 currentOpLeader :: riak_core_apl:preflist2()}).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
-start_link(From, SnapshotTime, Operations) ->
-    gen_fsm:start_link(?MODULE, [From, SnapshotTime, Operations], []).
+start_link(From, Transaction=#tx{}, Operations) ->
+    gen_fsm:start_link(?MODULE, [From, Transaction, Operations], []).
 
 finishOp(From, Key,Result) ->
    gen_fsm:send_event(From, {Key, Result}).
@@ -42,31 +43,47 @@ finishOp(From, Key,Result) ->
 %%%===================================================================
 
 %% @doc Initialize the s,,tate data.
-init([From, SnapshotTime, Operations]) ->
+init([From, ClientClock, Operations]) ->	
 
-%				if
-%                	lists:keymember(update, 1, Operations)  ->
-%                		TxType = update;
-%                	true ->
-                		TxType = read,
- %               end,
+
+	{Megasecs, Secs, Microsecs}=erlang:now(),
+	 
+	case (ClientClock > {Megasecs, Secs, Microsecs - ?DELTA}) of 
+		true->
+			%% should we wait?
+			{ClientMegasecs, ClientSecs, ClientMicrosecs}=ClientClock,
+			SnapshotTime = {ClientMegasecs, ClientSecs, ClientMicrosecs + ?MIN};
+
+		false ->
+			SnapshotTime = {Megasecs, Secs, Microsecs - ?DELTA}
+	end,
+		Transaction=#tx{snapshot_time=SnapshotTime, state=active, write_set=[]},
                 
     SD = #state{
                 from=From,
-                operations = Operations, 
-                snapshotTime=SnapshotTime,
-                txType=TxType 	
+                transaction=Transaction,
+                operations=Operations,
+                updated_partitions=[]
 		},
     {ok, prepare, SD, 0}.
 
 
 %% @doc Prepare the execution of the operation by obtaining an operation from the list,
 %%		and getting the leader of the operation.
-prepare(timeout, SD0=#state{operations=Operations}) ->
+prepare(timeout, SD0=#state{operations=Operations, transaction=Transaction, updated_partitions=UpdatedPartitions}) ->
 	case Operations of 
 	[] ->
-	    io:format("Executed all operations.~n"),
-	    {stop, normal, SD0};
+		case lists:lenght(UpdatedPartitions) of
+		0 ->
+			io:format("Executed all operations of a read-only transaction.~n"),
+			{stop, normal, SD0};
+		1 ->
+			[IndexNode]=UpdatedPartitions,
+			clockSI_vnode:local_commit(IndexNode, Transaction);
+		_ ->
+			{next_state, prepare_2PC, SD0, 0}		
+		end;
+		
 	[Op|TailOps] ->
 		[Op|TailOps] = Operations,
 		{_, Key,_} = Op, 
@@ -81,15 +98,69 @@ prepare(timeout, SD0=#state{operations=Operations}) ->
 %% @doc Contact the leader computed in the prepare state for it to execute the operation
 execute(timeout, SD0=#state{
                             currentOp=CurrentOp,
-               				snapshotTime=SnapshotTime,
+                            transaction=Transaction,
+                            updated_partitions=UpdatedPartitions,
                             currentOpLeader=CurrentOpLeader}) ->
     [OpType, Key, Param]=CurrentOp,                        
     io:format("Coord: Execute operation ~w ~w ~w~n",[OpType, Key, Param]),
 	io:format("Coord: Forward to node~w~n",[CurrentOpLeader]),
 	{IndexNode, _} = CurrentOpLeader,
-	clockSI_vnode:handleOp(IndexNode, self(), OpType, Key, Param, SnapshotTime), 
-    SD1 = SD0#state{},
+	
+	case OpType of read ->
+		clockSI_vnode:read_data_item(IndexNode, Transaction, Key),
+		SD1=SD0;
+	update ->
+		clockSI_vnode:update_data_item(IndexNode, Transaction#tx{write_set=[]}, Key, Param),
+		WriteSet= lists:append([Transaction#tx.write_set, [{CurrentOp}]]),
+		NewUpdatedPartitions= lists:append(UpdatedPartitions, [{IndexNode}]),
+		SD1 = SD0#state{transaction=Transaction#tx{write_set=WriteSet}, updated_partitions= NewUpdatedPartitions}
+	end,
     {next_state, prepare, SD1, ?INDC_TIMEOUT}.
+    
+    
+prepare_2PC(timeout, SD0=#state{transaction=Transaction, updated_partitions=UpdatedPartitions}) ->
+	TransactionLight=Transaction#tx{write_set=[], operations=[]},
+	clock_SI_vnode:prepare(TransactionLight, UpdatedPartitions),
+	NumToAck=lists:lenght(UpdatedPartitions),
+	{next_state, receive_prepared, SD0#state{transaction=TransactionLight, num_to_ack=NumToAck}, ?INDC_TIMEOUT}.
+	
+	
+
+receive_prepared({_Node, ReceivedPrepareTime}, S0=#state{num_to_ack= NumToAck, transaction=Transaction}) ->
+
+	PrepareTime = max(Transaction#tx.prepare_time, ReceivedPrepareTime),
+
+    case NumToAck of 1 -> 
+    	io:format("ClockSI: Finished collecting prepare replies, start committing..."),
+		{next_state, committing, S0=#state{transaction=#tx{prepare_time=coomit_time=PrepareTime, state=committing}}};
+
+	_ ->
+         io:format("ClockSI: Keep collecting prepare replies~n"),
+	{next_state, receive_prepared, S0#state{num_to_ack= NumToAck-1}, Transaction#tx{prepare_time=PrepareTime}}
+    end.
+    
+committing(timeout, SD0=#state{transaction=Transaction, updated_partitions=UpdatedPartitions}) -> 
+	clock_SI_vnode:commit(UpdatedPartitions, Transaction),
+	NumToAck=lists:lenght(UpdatedPartitions),
+	{next_state, receive_committed, SD0#state{num_to_ack=NumToAck, transaction=#tx{state=committed}}, ?INDC_TIMEOUT}.
+	
+
+receive_committed({_Node}, S0=#state{num_to_ack= NumToAck}) ->
+    case NumToAck of 1 -> 
+    	io:format("ClockSI: Finished collecting commit acks. Tx committed succesfully."),
+    	{stop, normal, S0};
+	_ ->
+         io:format("ClockSI: Keep collecting prepare replies~n"),
+		{next_state, receive_committed, S0#state{num_to_ack= NumToAck-1}}
+    end.
+	
+	
+
+	
+	
+	
+    
+
 
 
 handle_info(_Info, _StateName, StateData) ->
