5131a79bc48eb56107a23425f268933d2a3167f3
Annette Bieniusa <bieniusa@cs.uni-kl.de>
2014-05-22 11:59:46 +0200

    added more test cases

--- src/materializer.erl
+++ src/materializer.erl
@@ -31,10 +31,32 @@ update_snapshot(Type, Snapshot, [Op|Rest]) ->
     update_snapshot(Type, NewSnapshot, Rest).
     
 -ifdef(TEST).
-    materializer_test()->
-    	GCounter = create_snapshot(riak_dt_gcounter),
-	?assertEqual(0,riak_dt_gcounter:value(GCounter)),
-	Ops = [{1,#operation{payload={increment, actor1}}},{2,#operation{payload={increment, actor2}}},{3,#operation{payload={{increment, 3}, actor1}}},{4,#operation{payload={increment, actor3}}}],
-	GCounter2 = update_snapshot(riak_dt_gcounter, GCounter, Ops),
-	?assertEqual(6,riak_dt_gcounter:value(GCounter2)).
+
+%% @doc Testing gcounter with update log
+materializer_gcounter_withlog_test() ->
+    GCounter = create_snapshot(riak_dt_gcounter),
+    ?assertEqual(0,riak_dt_gcounter:value(GCounter)),
+    Ops = [{1,#operation{payload={increment, actor1}}},{2,#operation{payload={increment, actor2}}},{3,#operation{payload={{increment, 3}, actor1}}},{4,#operation{payload={increment, actor3}}}],
+    GCounter2 = update_snapshot(riak_dt_gcounter, GCounter, Ops),
+    ?assertEqual(6,riak_dt_gcounter:value(GCounter2)).
+
+%% @doc Testing gcounter with empty update log
+materializer_gcounter_emptylog_test() ->
+    GCounter = create_snapshot(riak_dt_gcounter),
+    ?assertEqual(0,riak_dt_gcounter:value(GCounter)),
+    Ops = [],
+    GCounter2 = update_snapshot(riak_dt_gcounter, GCounter, Ops),
+    ?assertEqual(0,riak_dt_gcounter:value(GCounter2)).
+
+%% @doc Testing non-existing crdt
+materializer_error_nocreate_test() ->
+    ?assertException(error, undef, create_snapshot(bla)).
+
+%% @doc Testing crdt with invalid update operation
+materializer_error_invalidupdate_test() ->
+    GCounter = create_snapshot(riak_dt_gcounter),
+    ?assertEqual(0,riak_dt_gcounter:value(GCounter)),
+    Ops = [{1,#operation{payload={decrement, actor1}}}],
+    ?assertException(error, function_clause, update_snapshot(riak_dt_gcounter, GCounter, Ops)).
+
 -endif.
