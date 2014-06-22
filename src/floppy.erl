243c488aab1dd5d1b4bd18a119252c2d09ec79a2
Annette Bieniusa <bieniusa@cs.uni-kl.de>
2014-06-23 00:05:40 +0200

    Started with fixing the API
    Added type spec and short description. Still quite a few open questions…

--- src/floppy.erl
+++ src/floppy.erl
@@ -5,12 +5,25 @@
 -export([append/2,
          read/2]).
 
+-type key() :: term().
+-type op()  :: term().
+-type crdt() :: term().
+-type val() :: term().
+-type reason() :: term().
+
 %% Public API
 
+%% @doc The append/2 function adds an operation to the log of the CRDT object stored at some key.
+%% TODO What is returned in case of success?!
+-spec append(key(), op()) -> {ok, term()} | {error, timeout}.
 append(Key, Op) ->
     lager:info("Append called!"),
     floppy_rep_vnode:append(Key, Op).
 
+
+%% @doc The read/2 function returns the current value for the CRDT object stored at some key.
+%% TODO Which state is exactly returned? Related to some snapshot? What is current?
+-spec read(key(), crdt()) -> val() | {error,reason()}.
 read(Key, Type) ->
     case floppy_rep_vnode:read(Key, Type) of
         {ok, Ops} ->
@@ -19,5 +32,5 @@ read(Key, Type) ->
             Type:value(Snapshot);
         {error, Reason} ->
             lager:info("Read failed: ~w~n", Reason),
-	    error
+	    {error, Reason}
     end.
