646abf85d667dcdbed4fdc3079311e02ac5cca43
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-07-08 12:13:54 -0400

    Adjust whitespace.

--- riak_test/append_list_test.erl
+++ riak_test/append_list_test.erl
@@ -16,17 +16,17 @@ confirm() ->
     append_list_test(Nodes).
 
 %% @doc Tests the append_list operation of the logging vnode
-%%      by triggering read repair. 
+%%      by triggering read repair.
 append_list_test(Nodes) ->
     [N1, _N2, N3, _N4, _N5, _N6] = Nodes,
     Key = key1,
-    
+
     DocIdx = rpc:call(N1, riak_core_util, chash_key, [{<<"floppy">>, Key}]),
     Preflist = rpc:call(N1, riak_core_apl, get_primary_apl, [DocIdx, 3, replication]),
     NodesRep = [Node || {{_Index, Node}, primary} <- Preflist],
     Excluded = hd(lists:reverse(NodesRep)),
     lager:info("Nodes that replicate ~w: ~w",[Key, NodesRep]),
-    
+
     F = fun(Node, Acc) ->
             case Node of
                 Excluded ->
@@ -35,17 +35,17 @@ append_list_test(Nodes) ->
                     [Node | Acc]
             end
         end,
-    
+
     Partition1 = lists:foldl(F, [], lists:reverse(Nodes)),
 
     WriteResult = rpc:call(N3, floppy, append, [Key, {increment, ucl}]),
     ?assertMatch({ok, _}, WriteResult),
 
     PartInfo = rt:partition(Partition1, [Excluded]),
-    
+
     Total = send_multiple_updates(N1, 10, 0, Key),
     lager:info("Total: ~w",[Total]),
-    
+
     ok = rt:heal(PartInfo),
     ok = rt:wait_for_cluster_service(Nodes, replication),
 
@@ -53,7 +53,7 @@ append_list_test(Nodes) ->
     lager:info("Value read: ~w",[Result]),
     ?assertEqual(Result, Total + 1),
     pass.
-    
+
 send_multiple_updates(Node, Total, Acc, Key) ->
     case Total of
         0 ->
