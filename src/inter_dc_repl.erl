441613f5040152f083336b331b063403c2a6cb1b
Annette Bieniusa <bieniusa@cs.uni-kl.de>
2014-07-13 23:34:32 +0200

    untabify

--- src/inter_dc_repl.erl
+++ src/inter_dc_repl.erl
@@ -3,25 +3,25 @@
 -include("floppy.hrl").
 
 %%gen_fsm call backs
--export([init/1, handle_event/3, handle_sync_event/4, 
-	handle_info/3, terminate/3, code_change/4]).
+-export([init/1, handle_event/3, handle_sync_event/4,
+         handle_info/3, terminate/3, code_change/4]).
 
 -export([ready/2, await_ack/2]).
 
 %%public api
 -export([propogate/1, propogate_sync/1, acknowledge/2]).
 
--record(state, 
-	{ otherdc,
+-record(state,
+        { otherdc,
           otherpid,
-	  payload,
-	  retries,
-	  from
-	  }).
+          payload,
+          retries,
+          from
+        }).
 
 -define(TIMEOUT,60000).
 
-%public api
+%%public api
 
 propogate_sync(Payload) ->
     Me = self(),
@@ -29,14 +29,14 @@ propogate_sync(Payload) ->
     _ = gen_fsm:start_link(?MODULE, [?OTHER_DC, inter_dc_recvr, Payload, {ReqId, Me}], []),
     receive
         {ReqId, normal} -> done;
-	{ReqId, Reason} -> Reason
+        {ReqId, Reason} -> Reason
     end.
 
 propogate(Payload) ->
     gen_fsm:start_link(?MODULE, [?OTHER_DC, inter_dc_recvr, Payload, ignore], []).
-   
+
 acknowledge(Pid, Payload) ->
-    gen_fsm:send_event(Pid, {ok, Payload}),   
+    gen_fsm:send_event(Pid, {ok, Payload}),
     ok.
 
 %% gen_fsm callbacks
@@ -47,18 +47,18 @@ init([OtherDC, OtherPid, Payload, From]) ->
 
 ready(timeout, StateData = #state{otherdc = Other, otherpid = PID, retries = RetryLeft, payload = Payload}) ->
     inter_dc_recvr:replicate({PID,Other}, Payload, {self(),node()}),
-    {next_state, await_ack, 
-	     StateData#state{retries = RetryLeft -1},
-	     ?TIMEOUT}.
+    {next_state, await_ack,
+     StateData#state{retries = RetryLeft -1},
+     ?TIMEOUT}.
 
 await_ack({ok,Payload}, StateData) ->
     lager:info("Replicated ~p ~n",[Payload]),
     {stop, normal, StateData};
 await_ack(timeout,StateData = #state{retries = RetryLeft}) ->
-    case RetryLeft of 
-	0 -> {stop, request_timeout, StateData};
-	_ ->  {next_state, ready, StateData, 0}
-    end.    
+    case RetryLeft of
+        0 -> {stop, request_timeout, StateData};
+        _ -> {next_state, ready, StateData, 0}
+    end.
 
 %% @private
 handle_event(_Event, _StateName, StateData) ->
@@ -76,12 +76,12 @@ handle_info(_Info, _StateName, StateData) ->
 
 %% @private
 terminate(Reason, _StateName, _State=#state{from = From}) ->
-    case From of 
-	{ReqId, Pid} ->  
-	    Pid ! {ReqId, Reason},
-	    Reason;
-	ignore ->
-	    Reason
+    case From of
+        {ReqId, Pid} ->
+            Pid ! {ReqId, Reason},
+            Reason;
+        ignore ->
+            Reason
     end.
 
 %% @private
