cfb8852a0889c750e8c0c920d600114b06f9f6b1
deepthidevaki <deepthidevaki@gmail.com>
2014-07-24 09:22:40 +0200

    fix tabs

--- src/floppy.erl
+++ src/floppy.erl
@@ -3,16 +3,16 @@
 -include_lib("riak_core/include/riak_core_vnode.hrl").
 
 -export([
-	 append/2,
-	 read/2,
-	 clocksi_execute_tx/2,
-	 clocksi_read/3,
-	 clocksi_bulk_update/2,
-	 clocksi_istart_tx/1,
-	 clocksi_iread/3,
-	 clocksi_iupdate/3,	 
-	 clocksi_iprepare/1,	 
-	 clocksi_icommit/1]).
+         append/2,
+         read/2,
+         clocksi_execute_tx/2,
+         clocksi_read/3,
+         clocksi_bulk_update/2,
+         clocksi_istart_tx/1,
+         clocksi_iread/3,
+         clocksi_iupdate/3,
+         clocksi_iprepare/1,
+         clocksi_icommit/1]).
 
 -type key() :: term().
 -type op()  :: term().
@@ -22,12 +22,13 @@
 
 %% Public API
 
-%% @doc The append/2 function adds an operation to the log of the CRDT object stored at some key.
+%% @doc The append/2 function adds an operation to the log of the CRDT object
+%% stored at some key.
 %% TODO What is returned in case of success?!
 -spec append(key(), op()) -> {ok, term()} | {error, timeout}.
 append(Key, Op) ->
     lager:info("Append called!"),
-    case clocksi_bulk_update(now(), [{update, Key, Op}]) of   
+    case clocksi_bulk_update(now(), [{update, Key, Op}]) of
         {ok, Result} ->
             {ok, Result};
         {error, Reason} ->
@@ -35,7 +36,8 @@ append(Key, Op) ->
     end.
 
 
-%% @doc The read/2 function returns the current value for the CRDT object stored at some key.
+%% @doc The read/2 function returns the current value for the CRDT object stored
+%% at some key.
 %% TODO Which state is exactly returned? Related to some snapshot? What is current?
 -spec read(key(), crdt()) -> val() | {error,reason()}.
 read(Key, Type) ->
@@ -50,45 +52,47 @@ read(Key, Type) ->
 
 %% @doc Starts a new ClockSI transaction.
 %% Input:
-%%	ClientClock: the last clock the client has seen from a successful transaction.
-%%	Operations: the list of the operations the transaction involves.
+%% ClientClock: last clock the client has seen from a successful transaction.
+%% Operations: the list of the operations the transaction involves.
 %% Returns:
-%%	an ok message along with the result of the read operations involved in the transaction, 
-%%	in case the tx ends successfully. 
-%%	error message in case of a failure.
+%% an ok message along with the result of the read operations involved in the
+%% the transaction, in case the tx ends successfully.
+%% error message in case of a failure.
 clocksi_execute_tx(ClientClock, Operations) ->
-    lager:info("FLOPPY: Received order to execute transaction with clock: ~w for the list of operations ~w ~n",
-               [ClientClock, Operations]),
-    {ok, _PID} = clocksi_tx_coord_sup:start_fsm([self(), ClientClock, Operations]),
+    lager:info(
+      "FLOPPY: Received transaction with clock: ~w for operations ~w ~n",
+      [ClientClock, Operations]),
+    {ok, _PID} = clocksi_tx_coord_sup:start_fsm(
+                   [self(), ClientClock, Operations]),
     receive
         EndOfTx ->
-	    lager:info("FLOPPY: TX completed!~n"),
-	    EndOfTx
+            lager:info("FLOPPY: TX completed!~n"),
+            EndOfTx
     after 10000 ->
-	    lager:info("FLOPPY: Tx failed!~n"),
-	    {error}
+            lager:info("FLOPPY: Tx failed!~n"),
+            {error}
     end.
-
 
 %% @doc Starts a new ClockSI interactive transaction.
 %% Input:
-%%	ClientClock: the last clock the client has seen from a successful transaction.
+%% ClientClock: last clock the client has seen from a successful transaction.
 %% Returns:
-%%	an ok message along with the new TxId. 
+%%	an ok message along with the new TxId.
 clocksi_istart_tx(Clock) ->
     lager:info("FLOPPY: Starting FSM for interactive transaction.~n"),
     case Clock of
         {Mega,Sec,Micro} ->
             ClientClock= clocksi_vnode:now_milisec({Mega,Sec,Micro})
     end,
-    {ok, _PID} = clocksi_interactive_tx_coord_sup:start_fsm([self(), ClientClock]),                                               
+    {ok, _PID} = clocksi_interactive_tx_coord_sup:start_fsm(
+                   [self(), ClientClock]),
     receive
         TxId ->
             lager:info("FLOPPY: TX started with TxId= ~w~n", [TxId]),
             TxId
     after 10000 ->
-	    lager:info("FLOPPY: Tx was not started!~n"),
-	    {error, timeout}
+            lager:info("FLOPPY: Tx was not started!~n"),
+            {error, timeout}
     end.
 
 
@@ -114,7 +118,3 @@ clocksi_iprepare(TxId=#tx_id{})->
 clocksi_icommit(TxId=#tx_id{})->
     {_, _, CoordFsmPid}=TxId,
     gen_fsm:sync_send_event(CoordFsmPid, commit).
-
-
-
-
--- src/floppy_app.erl
+++ src/floppy_app.erl
@@ -12,34 +12,38 @@
 start(_StartType, _StartArgs) ->
     case floppy_sup:start_link() of
         {ok, Pid} ->
-	    %%Log layer
+            %%Log layer
             ok = riak_core:register([{vnode_module, logging_vnode}]),
             ok = riak_core_node_watcher:service_up(logging, self()),
-       	    
-       	    %ClockSI layer
-     
-	    ok = riak_core:register([{vnode_module, clocksi_vnode}]),
-	    ok = riak_core_node_watcher:service_up(clocksi, self()),
 
-            ok = riak_core:register([{vnode_module, clocksi_downstream_generator_vnode}]),
-            ok = riak_core_node_watcher:service_up(clocksi_downstream_generator, self()),
+            %%ClockSI layer
+
+            ok = riak_core:register([{vnode_module, clocksi_vnode}]),
+            ok = riak_core_node_watcher:service_up(clocksi, self()),
+
+            ok = riak_core:register(
+                   [{vnode_module, clocksi_downstream_generator_vnode}]),
+            ok = riak_core_node_watcher:service_up(
+                   clocksi_downstream_generator, self()),
 
             ok = riak_core:register([{vnode_module, vectorclock_vnode}]),
             ok = riak_core_node_watcher:service_up(vectorclock, self()),
 
             ok = riak_core:register([{vnode_module, materializer_vnode}]),
             ok = riak_core_node_watcher:service_up(materializer, self()),
-                
-	    %%Within DC replication layer
-	    ok = riak_core:register([{vnode_module, floppy_rep_vnode}]),
+
+            %%Within DC replication layer
+            ok = riak_core:register([{vnode_module, floppy_rep_vnode}]),
             ok = riak_core_node_watcher:service_up(replication, self()),
 
-	    %%Inter DC replication layer
+            %%Inter DC replication layer
             ok = riak_core:register([{vnode_module, inter_dc_repl_vnode}]),
             ok = riak_core_node_watcher:service_up(interdcreplication, self()),
 
-            ok = riak_core_ring_events:add_guarded_handler(floppy_ring_event_handler, []),
-            ok = riak_core_node_watcher_events:add_guarded_handler(floppy_node_event_handler, []),
+            ok = riak_core_ring_events:add_guarded_handler(
+                   floppy_ring_event_handler, []),
+            ok = riak_core_node_watcher_events:add_guarded_handler(
+                   floppy_node_event_handler, []),
 
             {ok, Pid};
         {error, Reason} ->
