29ed825f3d2d232381266ee07d80ceacea15003c
balegas <v.sousa@campus.fct.unl.pt>
2014-08-06 15:23:25 +0100

    Walletapp after dialyzer

--- src/walletapp_pb.erl
+++ src/walletapp_pb.erl
@@ -1,66 +1,81 @@
 -module(walletapp_pb).
 
--export([credit/3, debit/3, init/3, getbalance/2, buyvoucher/3, usevoucher/3, readvouchers/2, createuser/3]).
+-export([credit/3,
+         debit/3,
+         getbalance/2,
+         buyvoucher/3,
+         usevoucher/3,
+         readvouchers/2]).
+
+-type reason() :: atom().
 
 %% walletapp uses floppystore apis - create, update, get
 
-%specify separate key for bal and voucher, could be put under same object later
-createuser(Keybal, Keyvoucher, Pid) ->
-    Result1 = init(Keybal, riak_dt_pncounter, Pid),
-    Result2 = init(Keyvoucher, riak_dt_orset, Pid), %which type to use for vouchers
-    [Result1 | Result2].
-
-init(Key, Type, Pid) ->
-    floppyc_pb_socket:get_crdt(Key,Type,Pid).
-    
+-spec credit(Key::term(), Amount::non_neg_integer(), Pid::pid()) -> ok | {error, reason()}.
 credit(Key, Amount, Pid) ->
     case floppyc_pb_socket:get_crdt(Key,riak_dt_pncounter,Pid) of
-        {ok,Counter} ->
+        {ok, Counter} ->
             CounterUpdt = floppyc_counter:increment(Amount, Counter),
-            floppyc_pb_socket:store_crdt(CounterUpdt,Pid);
+             case floppyc_pb_socket:store_crdt(CounterUpdt,Pid) of
+                ok -> ok;
+                {error,Reason} -> {error, Reason}
+             end;
         {error, Reason} -> 
              {error, Reason}
     end.
 
+-spec debit(Key::term(), Amount::non_neg_integer(), Pid::pid()) -> ok | {error, reason()}.
 debit(Key, Amount, Pid) ->
     case floppyc_pb_socket:get_crdt(Key,riak_dt_pncounter,Pid) of
         {ok,Counter} ->
             CounterUpdt = floppyc_counter:decrement(Amount, Counter),
-            floppyc_pb_socket:store_crdt(CounterUpdt,Pid);
+            case floppyc_pb_socket:store_crdt(CounterUpdt,Pid) of
+                ok -> ok;
+                {error,Reason} -> {error, Reason} 
+            end;
         {error, Reason} -> 
             {error, Reason}
     end.
 
+-spec getbalance(Key::term(), Pid::pid()) -> {error, error_in_read} | {ok, integer()}.
 getbalance(Key, Pid) ->
     case floppyc_pb_socket:get_crdt(Key,riak_dt_pncounter,Pid) of
         {ok,Counter} ->
-            floppyc_counter:value(Counter);
-        {error, Reason} -> 
-            {error, Reason}
+            {ok,floppyc_counter:value(Counter)};
+        {error, _Reason} -> 
+            {error, error_in_read}
     end.
 
+-spec buyvoucher(Key::term(),Voucher::term(), Pid::pid()) -> ok | {error, reason()}.
 buyvoucher(Key, Voucher, Pid) ->
     case floppyc_pb_socket:get_crdt(Key,riak_dt_orset,Pid) of
         {ok,Set} ->
             SetUpdt = floppyc_set:add(Voucher,Set),
-            floppyc_pb_socket:store_crdt(SetUpdt,Pid);
+            case floppyc_pb_socket:store_crdt(SetUpdt,Pid) of
+               ok -> ok;
+               {error, Reason} -> {error, Reason}
+           end;
         {error, Reason} -> 
             {error, Reason}
     end.
-
+-spec usevoucher(Key::term(),Voucher::term(),Pid::pid()) -> ok | {error, reason()}.
 usevoucher(Key, Voucher, Pid) ->
     case floppyc_pb_socket:get_crdt(Key,riak_dt_orset,Pid) of
         {ok,Set} ->
             SetUpdt = floppyc_set:remove(Voucher,Set),
-            floppyc_pb_socket:store_crdt(SetUpdt,Pid);
+           case floppyc_pb_socket:store_crdt(SetUpdt,Pid) of
+               ok -> ok;
+               {error, Reason} -> {error, Reason}
+           end;
         {error, Reason} -> 
             {error, Reason}
     end.
 
+-spec readvouchers(Key::term(), Pid::pid()) -> {ok, list()} | {error, reason()}.
 readvouchers(Key, Pid) ->
     case floppyc_pb_socket:get_crdt(Key,riak_dt_orset,Pid) of
         {ok,Set} ->
-            sets:to_list(floppyc_set:value(Set));
+            {ok,sets:to_list(floppyc_set:value(Set))};
         {error, Reason} -> 
             {error, Reason}
     end. 
--- src/walletclient_pb.erl
+++ src/walletclient_pb.erl
@@ -1,64 +1,100 @@
 -module(walletclient_pb).
+-include("floppy.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
--export([run/2]).
+-export([run/1, start/1]).
 
-run(Key1,Key2)->
-    net_kernel:start(['walletclient@127.0.0.1', longnames]),
-    erlang:set_cookie(node(),floppy),
-    %Created = walletapp:createuser(Key1,Key2),
-    {ok,Pid} = floppyc_pb_socket:start("localhost",8087),
-    Result1 = testc(Key1, 10, [bal],Pid),
-    Result2 = testvoucher(Key2,10,[voucher],Pid),
-    [Result1 | Result2].
-       
-testc(_, 0, Result, _Pid) ->
+start(Args) ->
+    [Key1, Key2] = Args,
+    run([Key1,Key2]).
+
+-spec run([key()]) -> ok | {error, reason()}.
+run([Key_bal,Key_voucher])->
+    io:format("Starting Wallet Client~n"),
+    case floppyc_pb_socket:start_link("localhost",8087) of
+        {ok, Pid} ->
+            Result1 = testbalance(Key_bal, 10, [],Pid),
+            io:format("~nTesting credit and debit operations: ~p ~n ", [Result1]),
+            Result2 = testvoucher(Key_voucher,10,[],Pid),
+            io:format("~nTesting voucher use and buy operations: ~p ~n", [Result2]),
+            ok;
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+-spec testbalance(key(), pos_integer(), [any()],pid()) -> [any()].
+testbalance(_, 0, Result,_Pid) ->
     lists:reverse(Result);
-testc(Key, N, Result, Pid) ->
-    {A1,A2,A3} = now(),
-    random:seed(A1, A2, A3),
-    Result1 = testcredit(Key, random:uniform(100), Pid),
-    Result2 = testdebit(Key, random:uniform(100), Pid),
-    %Result1 = testcredit(Key, 2),
-    %Result2 = testdebit(Key,1),
+testbalance(Key, N, Result,Pid) ->
+    {A1, A2, A3} = now(),
+    _ = random:seed(A1, A2, A3),
+    Result1 = testcredit(Key, random:uniform(100),Pid),
+    Result2 = testdebit(Key, random:uniform(100),Pid),
     timer:sleep(10),
-    testc(Key, N-1,[Result2 | [Result1 | Result]], Pid).
+    testbalance(Key, N-1, [Result2 | [Result1 | Result]],Pid).
 
+-spec testcredit(key(), pos_integer(), pid()) -> ok | {error, reason()}.
 testcredit(Key, Amount, Pid) ->
-    Balbefore = walletapp_pb:getbalance(Key, Pid),
-    walletapp_pb:credit(Key, Amount, Pid),
-    case walletapp_pb:getbalance(Key, Pid) of
-        {error,Reason} ->
-            {error,Reason};
-        Val ->
-            ?assert(Val =:= Balbefore + Amount)
-            %{ok, credit, Balbefore, Amount, Val}
+    {ok, Balbefore} = walletapp_pb:getbalance(Key, Pid),
+    case walletapp_pb:credit(Key, Amount, Pid) of
+        {error, Reason} ->
+            {error, Reason};
+        ok ->
+            case walletapp_pb:getbalance(Key, Pid) of
+                {error, Reason} ->
+                    {error, Reason};
+                {ok, Val} ->
+                    Val = Balbefore + Amount,
+                    ok
+            end
     end.
 
+-spec testdebit(key(), pos_integer(), pid()) -> ok | {error, reason()}.
 testdebit(Key, Amount, Pid) ->
-    Balbefore = walletapp_pb:getbalance(Key, Pid),
-    walletapp_pb:debit(Key, Amount, Pid),
-    case walletapp_pb:getbalance(Key, Pid) of
-        {error,Reason} ->
-            {error,Reason};
-        Val ->
-            ?assert(Val =:= Balbefore - Amount)
-            %{ok, debit, Balbefore, Amount, Val}
+    case walletapp_pb:getbalance(Key,Pid) of
+        {ok, Balbefore} ->
+            case walletapp_pb:debit(Key, Amount,Pid) of
+                ok ->
+                    case walletapp_pb:getbalance(Key,Pid) of
+                        {error, Reason} ->
+                            {error, Reason};
+                        {ok, Val} ->
+                            Val = Balbefore - Amount,
+                            ok
+                    end;
+                {error, Reason} ->
+                    {error, Reason}
+            end;
+        {error, Reason} ->
+            {error, Reason}
     end.
 
+-spec testvoucher(key(), pos_integer(), [any()], pid()) -> [any()].
 testvoucher(_,0,Result, _Pid) ->
     lists:reverse(Result);
+
 testvoucher(Key, N, Result, Pid) ->
     {A1,A2,A3} = now(),
-    random:seed(A1, A2, A3),
+    _Seed = random:seed(A1, A2, A3),
     Result1 = testadd(Key, random:uniform(100), Pid),
     timer:sleep(10),
     testvoucher(Key, N-1,[Result1 | Result], Pid).
 
+-spec testadd(key(), pos_integer(), pid()) -> ok | {error, reason()}.
 testadd(Key, Voucher, Pid) ->
-    Init = walletapp_pb:readvouchers(Key, Pid),
-    walletapp_pb:buyvoucher(Key,Voucher, Pid),
-    After = walletapp_pb:readvouchers(Key, Pid),
-    ?assert(lists:usort([Voucher| Init])=:= lists:sort(After)).
-%{ok, add, Init, Voucher, After}.
+    case walletapp_pb:readvouchers(Key,Pid) of
+        {error, Reason} ->
+            {error, Reason};
+        {ok, Init} ->
+            case walletapp_pb:buyvoucher(Key,Voucher,Pid) of
+                {error, Reason} ->
+                    {error, Reason};
+                ok ->
+                    {ok, After} = walletapp_pb:readvouchers(Key,Pid),
+                    UniqueSorted = lists:usort([Voucher|Init]),
+                    Sorted = lists:sort(After),
+                    UniqueSorted = Sorted,
+                    ok
+            end
+    end.
 
