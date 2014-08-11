651aa06dd56a36750db54bd03d7c0430487848bc
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-08-10 21:02:03 -0700

    Return proper response.
    Return the proper response so the harness doesn’t return a false
    failure.

--- riak_test/pb_client_test.erl
+++ riak_test/pb_client_test.erl
@@ -11,44 +11,49 @@
 -define(PORT, 10017).
 
 confirm() ->
-    start_stop_test(),
-    get_empty_crdt_test(<<"key0">>),
-    update_counter_crdt_test(<<"key1">>,10),
-    update_counter_crdt_and_read_test(<<"key2">>,15).
+    [Nodes] = rt:build_clusters([1]),
+
+    lager:info("Waiting for ring to converge."),
+    rt:wait_until_ring_converged(Nodes),
+
+    pass = start_stop_test(),
+    pass = get_empty_crdt_test(<<"key0">>),
+    pass = update_counter_crdt_test(<<"key1">>, 10),
+    pass = update_counter_crdt_and_read_test(<<"key2">>, 15),
+
+    pass.
 
 start_stop_test() ->
-    [_Nodes] = rt:build_clusters([1]),
+    lager:info("Verifying pb connection..."),
     {ok, Pid} = floppyc_pb_socket:start(?ADDRESS, ?PORT),
     Disconnected = floppyc_pb_socket:stop(Pid),
-    ?assertMatch(ok,Disconnected),
+    ?assertMatch(ok, Disconnected),
     pass.
 
 get_empty_crdt_test(Key) ->
-    {ok,Pid} = floppyc_pb_socket:start(?ADDRESS, ?PORT),
-    {ok,Obj} = floppyc_pb_socket:get_crdt(Key,riak_dt_pncounter,Pid),
+    {ok, Pid} = floppyc_pb_socket:start(?ADDRESS, ?PORT),
+    {ok, Obj} = floppyc_pb_socket:get_crdt(Key, riak_dt_pncounter, Pid),
     _Disconnected = floppyc_pb_socket:stop(Pid),
-    ?assertMatch(true,floppyc_counter:is_type(Obj)),
+    ?assertMatch(true, floppyc_counter:is_type(Obj)),
     pass.
 
-update_counter_crdt_test(Key,Amount) ->
-    {ok,Pid} = floppyc_pb_socket:start(?ADDRESS, ?PORT),
-    {ok,Obj} = floppyc_pb_socket:get_crdt(Key,riak_dt_pncounter,Pid),
-    Obj2 = floppyc_counter:increment(Amount,Obj),
-    Result = floppyc_pb_socket:store_crdt(Obj2,Pid),
+update_counter_crdt_test(Key, Amount) ->
+    {ok, Pid} = floppyc_pb_socket:start(?ADDRESS, ?PORT),
+    {ok, Obj} = floppyc_pb_socket:get_crdt(Key, riak_dt_pncounter, Pid),
+    Obj2 = floppyc_counter:increment(Amount, Obj),
+    Result = floppyc_pb_socket:store_crdt(Obj2, Pid),
      _Disconnected = floppyc_pb_socket:stop(Pid),
-    ?assertMatch(ok,Result).
+    ?assertMatch(ok, Result),
+    pass.
 
-update_counter_crdt_and_read_test(Key,Amount) ->
-    update_counter_crdt_test(Key,Amount),
-    get_crdt_check_value(Key,riak_dt_pncounter,Amount).
+update_counter_crdt_and_read_test(Key, Amount) ->
+    pass = update_counter_crdt_test(Key, Amount),
+    pass = get_crdt_check_value(Key, riak_dt_pncounter, Amount).
 
-get_crdt_check_value(Key,Type,Expected) ->
-    {ok,Pid} = floppyc_pb_socket:start(?ADDRESS, ?PORT),
-    {ok,Obj} = floppyc_pb_socket:get_crdt(Key,Type,Pid),
+get_crdt_check_value(Key, Type, Expected) ->
+    {ok, Pid} = floppyc_pb_socket:start(?ADDRESS, ?PORT),
+    {ok, Obj} = floppyc_pb_socket:get_crdt(Key, Type, Pid),
     Mod = floppyc_datatype:module_for_type(Type),
     _Disconnected = floppyc_pb_socket:stop(Pid),
-    ?assertMatch(Expected, Mod:value(Obj)).
-
-
-%%TODO: Add incorrect type cast test
-
+    ?assertMatch(Expected, Mod:value(Obj)),
+    pass.
