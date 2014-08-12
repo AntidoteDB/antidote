7295ff943cdd18ff33520153129b8af3dfeca349
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2014-08-12 14:02:05 -0700

    Fix single value tuple.
    Don’t use a single value tuple; use jut the atom alone.

--- src/floppy.erl
+++ src/floppy.erl
@@ -18,7 +18,7 @@
 %% @doc The append/2 function adds an operation to the log of the CRDT
 %%      object stored at some key.
 append(Key, Type, {OpParam, Actor}) ->
-    Operations = [update, Key, Type, {OpParam, Actor}],
+    Operations = [{update, Key, Type, {OpParam, Actor}}],
     case clocksi_execute_tx(Operations) of
         {ok, Result} ->
             {ok, Result};
@@ -30,7 +30,7 @@ append(Key, Type, {OpParam, Actor}) ->
 %%      object stored at some key.
 read(Key, Type) ->
     case clocksi_read(now(), Key, Type) of
-        {ok,{_,[Val],_ }} ->
+        {ok,{_, [Val], _}} ->
             {ok, Val};
         {error, Reason} ->
             {error, Reason}
@@ -39,64 +39,60 @@ read(Key, Type) ->
 %% Clock SI API
 
 %% @doc Starts a new ClockSI transaction.
-%% Input:
-%% ClientClock: last clock the client has seen from a successful transaction.
-%% Operations: the list of the operations the transaction involves.
-%% Returns:
-%% an ok message along with the result of the read operations involved in the
-%% the transaction, in case the tx ends successfully.
-%% error message in case of a failure.
+%%      Input:
+%%      ClientClock: last clock the client has seen from a successful transaction.
+%%      Operations: the list of the operations the transaction involves.
+%%      Returns:
+%%      an ok message along with the result of the read operations involved in the
+%%      the transaction, in case the tx ends successfully.
+%%      error message in case of a failure.
+%%
 clocksi_execute_tx(Clock, Operations) ->
     lager:info(
       "FLOPPY: Received transaction with clock: ~w for operations ~w ~n",
       [Clock, Operations]),
-    case Clock of
+    ClientClock = case Clock of
         {Mega,Sec,Micro} ->
-            ClientClock= clocksi_vnode:now_milisec({Mega,Sec,Micro});
+            clocksi_vnode:now_milisec({Mega,Sec,Micro});
         _ ->
-            ClientClock = Clock
+            Clock
     end,
-    {ok, _PID} = clocksi_tx_coord_sup:start_fsm(
-                   [self(), ClientClock, Operations]),
+    {ok, _} = clocksi_tx_coord_sup:start_fsm([self(), ClientClock, Operations]),
     receive
         EndOfTx ->
             lager:info("FLOPPY: TX completed!~n"),
             EndOfTx
     after 10000 ->
             lager:info("FLOPPY: Tx failed!~n"),
-            {error}
+            error
     end.
 
 clocksi_execute_tx(Operations) ->
-    lager:info(
-      "FLOPPY: Received transaction for operations ~w ~n",
-      [Operations]),
-    {ok, _PID} = clocksi_tx_coord_sup:start_fsm(
-                   [self(), Operations]),
+    lager:info("FLOPPY: Received transaction for operations ~w ~n", [Operations]),
+    {ok, _} = clocksi_tx_coord_sup:start_fsm([self(), Operations]),
     receive
         EndOfTx ->
             lager:info("FLOPPY: TX completed!~n"),
             EndOfTx
     after 10000 ->
             lager:info("FLOPPY: Tx failed!~n"),
-            {error}
+            error
     end.
 
 %% @doc Starts a new ClockSI interactive transaction.
-%% Input:
-%% ClientClock: last clock the client has seen from a successful transaction.
-%% Returns:
-%%	an ok message along with the new TxId.
+%%      Input:
+%%      ClientClock: last clock the client has seen from a successful transaction.
+%%      Returns: an ok message along with the new TxId.
+%%
 clocksi_istart_tx(Clock) ->
     lager:info("FLOPPY: Starting FSM for interactive transaction.~n"),
-    case Clock of
-        {Mega,Sec,Micro} ->
-            ClientClock= clocksi_vnode:now_milisec({Mega,Sec,Micro});
+    ClientClock = case Clock of
+        {Mega, Sec, Micro} ->
+            clocksi_vnode:now_milisec({Mega, Sec, Micro});
         _ ->
-            ClientClock = Clock
+            Clock
     end,
-    {ok, _PID} = clocksi_interactive_tx_coord_sup:start_fsm(
-                   [self(), ClientClock]),
+    {ok, _} = clocksi_interactive_tx_coord_sup:start_fsm([self(), ClientClock]),
     receive
         TxId ->
             lager:info("FLOPPY: TX started with TxId= ~w~n", [TxId]),
@@ -108,8 +104,7 @@ clocksi_istart_tx(Clock) ->
 
 clocksi_istart_tx() ->
     lager:info("FLOPPY: Starting FSM for interactive transaction.~n"),
-    {ok, _PID} = clocksi_interactive_tx_coord_sup:start_fsm(
-                   [self()]),
+    {ok, _} = clocksi_interactive_tx_coord_sup:start_fsm([self()]),
     receive
         TxId ->
             lager:info("FLOPPY: TX started with TxId= ~w~n", [TxId]),
@@ -126,21 +121,16 @@ clocksi_bulk_update(Operations) ->
     clocksi_execute_tx(Operations).
 
 clocksi_read(ClientClock, Key, Type) ->
-    Operation={read, Key, Type},
-    clocksi_execute_tx(ClientClock, [Operation]).
+    clocksi_execute_tx(ClientClock, [{read, Key, Type}]).
 
-clocksi_iread(TxId, Key, Type) ->
-    {_, _, CoordFsmPid}=TxId,
+clocksi_iread({_, _, CoordFsmPid}, Key, Type) ->
     gen_fsm:sync_send_event(CoordFsmPid, {read, {Key, Type}}).
 
-clocksi_iupdate(TxId, Key, Type, OpParams) ->
-    {_, _, CoordFsmPid}=TxId,
+clocksi_iupdate({_, _, CoordFsmPid}, Key, Type, OpParams) ->
     gen_fsm:sync_send_event(CoordFsmPid, {update, {Key, Type, OpParams}}).
 
-clocksi_iprepare(TxId)->
-    {_, _, CoordFsmPid}=TxId,
+clocksi_iprepare({_, _, CoordFsmPid})->
     gen_fsm:sync_send_event(CoordFsmPid, {prepare, empty}).
 
-clocksi_icommit(TxId)->
-    {_, _, CoordFsmPid}=TxId,
+clocksi_icommit({_, _, CoordFsmPid})->
     gen_fsm:sync_send_event(CoordFsmPid, commit).
