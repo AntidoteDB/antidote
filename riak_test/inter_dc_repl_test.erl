7bd1362f6be2c2e1d0f952643926b8f323df06f7
deepthidevaki <deepthidevaki@gmail.com>
2014-09-26 10:42:44 +0200

    add a test for causality using orset

--- riak_test/inter_dc_repl_test.erl
+++ riak_test/inter_dc_repl_test.erl
@@ -17,6 +17,7 @@ confirm() ->
     rt:wait_until_ring_converged(Cluster2),
     simple_replication_test(Cluster1, Cluster2),
     multiple_keys_test(Cluster1, Cluster2),
+    causality_test(Cluster1,Cluster2),
     pass.
 
 simple_replication_test(Cluster1, Cluster2) ->
@@ -44,6 +45,7 @@ simple_replication_test(Cluster1, Cluster2) ->
                            [CommitTime, key1, riak_dt_gcounter]),
     {ok, {_,[ReadSet],_} }= ReadResult,
     ?assertEqual(3, ReadSet),
+    lager:info("Simple replication test passed!"),
     pass.
 
 multiple_keys_test(Cluster1, Cluster2) ->
@@ -62,7 +64,9 @@ multiple_keys_test(Cluster1, Cluster2) ->
     Result1 = multiple_reads(Node1, 1, 10, 10,CommitTime),
     ?assertEqual(length(Result1), 0),
     Result2 = multiple_reads(Node2, 1, 10, 10, CommitTime),
-    ?assertEqual(length(Result2), 0).
+    ?assertEqual(length(Result2), 0),
+    lager:info("Multiple key read-write test passed!"),
+    pass.
 
 multiple_writes(Node, Start, End, Actor)->
     F = fun(N, Acc) ->
@@ -88,3 +92,34 @@ multiple_reads(Node, Start, End, Total, CommitTime) ->
             end
     end,
     lists:foldl(F, [], lists:seq(Start, End)).
+
+causality_test(Cluster1, Cluster2) ->
+    %% add element e to orset in one DC
+    %% remove element e from other DC
+    %% result set should not contain e
+    Node1 = hd(Cluster1),
+    Node2 = hd(Cluster2),
+    Key = myset,
+    %% Add two elements in DC1
+    AddResult1 = rpc:call(Node1,
+                           floppy, append,
+                           [Key, riak_dt_orset, {{add, first}, act1}]),
+    ?assertMatch({ok, _}, AddResult1),
+    AddResult2 = rpc:call(Node1,
+                           floppy, append,
+                           [Key, riak_dt_orset, {{add, second}, act1}]),
+    ?assertMatch({ok, _}, AddResult2),
+    {ok,{_,_,CommitTime}}=AddResult2,
+
+    %% Remove one element from D2C
+    RemoveResult = rpc:call(Node2,
+                           floppy, clocksi_bulk_update,
+                            [CommitTime,
+                             [{update, Key, riak_dt_orset, {{remove, first}, act1}}]]),
+    ?assertMatch({ok, _}, RemoveResult),
+    %% Read result
+    Result = rpc:call(Node2, floppy, read,
+                           [Key, riak_dt_orset]),
+    ?assertMatch({ok, [second]}, Result),
+    lager:info("Causality test passed!"),
+    pass.
