68fc3c6bb0eef3a6fdd8609b004e13bc9ed3264d
Zhongmiao Li <mars.leezm@gmail.com>
2014-11-24 18:51:04 +0000

    Add comments

--- src/clocksi_materializer.erl
+++ src/clocksi_materializer.erl
@@ -127,19 +127,11 @@ materialize_eager(Type, Snapshot, [Op|Rest]) ->
 
 -ifdef(TEST).
 
-<<<<<<< HEAD
 materializer_clocksi_test()->
-    PNCounter = create_snapshot(crdt_pncounter),
+    PNCounter = new(crdt_pncounter),
     ?assertEqual(0,crdt_pncounter:value(PNCounter)),
     Op1 = #clocksi_payload{key = abc, type = crdt_pncounter,
                            op_param = {update,{{increment,2},1}},
-=======
-materializer_clocksi_sequential_test() ->
-    GCounter = new(riak_dt_gcounter),
-    ?assertEqual(0,riak_dt_gcounter:value(GCounter)),
-    Op1 = #clocksi_payload{key = abc, type = riak_dt_gcounter,
-                           op_param = {{increment,2}, actor1},
->>>>>>> origin/materialiser_fix
                            commit_time = {1, 1}, txid = 1},
     Op2 = #clocksi_payload{key = abc, type = crdt_pncounter,
                            op_param = {update,{{increment,1},1}},
@@ -152,66 +144,56 @@ materializer_clocksi_sequential_test() ->
                            commit_time = {1, 4}, txid = 4},
 
     Ops = [Op1,Op2,Op3,Op4],
-<<<<<<< HEAD
-    {ok, PNCounter2} = update_snapshot(crdt_pncounter,
+    {ok, PNCounter2, CommitTime2} = materialize(crdt_pncounter,
                                       PNCounter, vectorclock:from_list([{1,3}]),
-                                      Ops, ignore),
-    ?assertEqual(4,crdt_pncounter:value(PNCounter2)),
-    {ok, PNcounter3} = get_snapshot(crdt_pncounter,
-                                   vectorclock:from_list([{1,4}]),Ops),
-    ?assertEqual(6,crdt_pncounter:value(PNcounter3)).
-=======
-    {ok, GCounter2, CommitTime2} = materialize(riak_dt_gcounter,
-                                      GCounter, vectorclock:from_list([{1,3}]),
                                       Ops, ignore, ignore),
-    ?assertEqual({4, {1,3}}, {riak_dt_gcounter:value(GCounter2), CommitTime2}),
-    Snapshot=new(riak_dt_gcounter),
-    {ok, Gcounter3, CommitTime3} = materialize(riak_dt_gcounter, Snapshot,
+    ?assertEqual({4, {1,3}}, {crdt_pncounter:value(PNCounter2), CommitTime2}),
+    Snapshot=new(crdt_pncounter),
+    {ok, PNcounter3, CommitTime3} = materialize(crdt_pncounter, Snapshot,
                                    vectorclock:from_list([{1,4}]),Ops, ignore),
-    ?assertEqual({6, {1,4}}, {riak_dt_gcounter:value(Gcounter3), CommitTime3}),
-    {ok, Gcounter4, CommitTime4} = materialize(riak_dt_gcounter, Snapshot,
+    ?assertEqual({6, {1,4}}, {crdt_pncounter:value(PNcounter3), CommitTime3}),
+    {ok, PNcounter4, CommitTime4} = materialize(crdt_pncounter, Snapshot,
                                    vectorclock:from_list([{1,7}]),Ops, ignore),
-    ?assertEqual({6, {1,4}}, {riak_dt_gcounter:value(Gcounter4), CommitTime4}).
+    ?assertEqual({6, {1,4}}, {crdt_pncounter:value(PNcounter4), CommitTime4}).
 
 materializer_clocksi_concurrent_test() ->
-    GCounter = new(riak_dt_gcounter),
-    ?assertEqual(0,riak_dt_gcounter:value(GCounter)),
-    Op1 = #clocksi_payload{key = abc, type = riak_dt_gcounter,
-                           op_param = {{increment,2}, actor1},
+    PNCounter = new(crdt_pncounter),
+    ?assertEqual(0,crdt_pncounter:value(PNCounter)),
+    Op1 = #clocksi_payload{key = abc, type = crdt_pncounter,
+                           op_param = {update, {{increment,2}, actor1}},
                            commit_time = {1, 1}, txid = 1},
-    Op2 = #clocksi_payload{key = abc, type = riak_dt_gcounter,
-                           op_param = {{increment,1}, actor1},
+    Op2 = #clocksi_payload{key = abc, type = crdt_pncounter,
+                           op_param = {update, {{increment,1}, actor1}},
                            commit_time = {1, 2}, txid = 2},
-    Op3 = #clocksi_payload{key = abc, type = riak_dt_gcounter,
-                           op_param = {{increment,1}, actor1},
+    Op3 = #clocksi_payload{key = abc, type = crdt_pncounter,
+                           op_param = {update, {{increment,1}, actor1}},
                            commit_time = {2, 1}, txid = 3},
->>>>>>> origin/materialiser_fix
 
     Ops = [Op1,Op2,Op3],
-    {ok, GCounter2, CommitTime2} = materialize(riak_dt_gcounter,
-                                      GCounter,
+    {ok, PNCounter2, CommitTime2} = materialize(crdt_pncounter,
+                                      PNCounter,
                                       vectorclock:from_list([{2,2},{1,2}]),
                                       Ops, ignore, ignore),
-    ?assertEqual({4, {2,1}}, {riak_dt_gcounter:value(GCounter2), CommitTime2}),
+    ?assertEqual({4, {2,1}}, {crdt_pncounter:value(PNCounter2), CommitTime2}),
     
     
     
-    Snapshot=new(riak_dt_gcounter),
-    {ok, Gcounter3, CommitTime3} = materialize(riak_dt_gcounter, Snapshot,
+    Snapshot=new(crdt_pncounter),
+    {ok, PNcounter3, CommitTime3} = materialize(crdt_pncounter, Snapshot,
                                    vectorclock:from_list([{1,2}]),Ops, ignore),
-    ?assertEqual({3, {1,2}}, {riak_dt_gcounter:value(Gcounter3), CommitTime3}),
+    ?assertEqual({3, {1,2}}, {crdt_pncounter:value(PNcounter3), CommitTime3}),
     
-    {ok, Gcounter4, CommitTime4} = materialize(riak_dt_gcounter, Snapshot,
+    {ok, PNcounter4, CommitTime4} = materialize(crdt_pncounter, Snapshot,
                                    vectorclock:from_list([{2,1}]),Ops, ignore),
-    ?assertEqual({1, {2,1}}, {riak_dt_gcounter:value(Gcounter4), CommitTime4}).
+    ?assertEqual({1, {2,1}}, {crdt_pncounter:value(PNcounter4), CommitTime4}).
 
 %% @doc Testing gcounter with empty update log
 materializer_clocksi_noop_test() ->
-    GCounter = new(riak_dt_gcounter),
-    ?assertEqual(0,riak_dt_gcounter:value(GCounter)),
+    PNCounter = new(crdt_pncounter),
+    ?assertEqual(0,crdt_pncounter:value(PNCounter)),
     Ops = [],
-    {ok, GCounter2, ignore} = materialize(riak_dt_gcounter, GCounter,
+    {ok, PNCounter2, ignore} = materialize(crdt_pncounter, PNCounter,
                                 vectorclock:from_list([{1,1}]),
                                 Ops, ignore, ignore),
-    ?assertEqual(0,riak_dt_gcounter:value(GCounter2)).
+    ?assertEqual(0,crdt_pncounter:value(PNCounter2)).
 -endif.
