fca6e83a7215046da850f691bca4018e595558f0
deepthidevaki <deepthidevaki@gmail.com>
2015-03-03 13:49:19 +0100

    inter dc communication: some more docs and specs

--- src/inter_dc_manager.erl
+++ src/inter_dc_manager.erl
@@ -1,5 +1,27 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2014 SyncFree Consortium.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
 -module(inter_dc_manager).
 -behaviour(gen_server).
+
+-include("antidote.hrl").
 
 -export([start_link/0,
          get_my_dc/0,
@@ -26,18 +48,28 @@ start_link() ->
 get_my_dc() ->
     gen_server:call(?MODULE, get_my_dc, infinity).
 
+%% Starts listening to TCP port for incomming requests from other DCs
+%% Returns the address of the DC, which could be used by others to communicate
+-spec start_receiver(port()) -> {ok, dc_address()}.
 start_receiver(Port) ->
     gen_server:call(?MODULE, {start_receiver, Port}, infinity).
 
+%% Returns all DCs known to this DC.
+-spec get_dcs() ->{ok, [dc_address()]}.
 get_dcs() ->
     gen_server:call(?MODULE, get_dcs, infinity).
 
+%% Add info about a new DC. This info could be
+%% used by other modules to communicate to other DC
+-spec add_dc(dc_address()) -> ok.
 add_dc(NewDC) ->
     gen_server:call(?MODULE, {add_dc, NewDC}, infinity).
 
+%% Add a list of DCs to this DC
+-spec add_list_dcs([dc_address()]) -> ok.
 add_list_dcs(DCs) ->
     gen_server:call(?MODULE, {add_list_dcs, DCs}, infinity).
-   
+
 
 %% ===================================================================
 %% gen_server callbacks
