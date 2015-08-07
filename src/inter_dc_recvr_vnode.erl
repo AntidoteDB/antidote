38784e0aec44b327413cf5f21594916b49b18da3
root <root@suno-45.sophia.grid5000.fr>
2015-08-07 22:50:40 +0200

    Removing dets

--- src/inter_dc_recvr_vnode.erl
+++ src/inter_dc_recvr_vnode.erl
@@ -84,23 +84,25 @@ store_update(Node, Transaction) ->
 
 %% riak_core_vnode call backs
 init([Partition]) ->
-    StateFile = string:concat(integer_to_list(Partition), "replstate"),
-    Path = filename:join(
-             app_helper:get_env(riak_core, platform_data_dir), StateFile),
-    case dets:open_file(StateFile, [{file, Path}, {type, set}]) of
-        {ok, StateStore} ->
-            case dets:lookup(StateStore, recvr_state) of
-                %%If file already exists read previous state from it.
-                [{recvr_state, State}] ->
-                    {ok, State};
-                [] ->
-                    {ok, State } = inter_dc_repl_update:init_state(Partition),
-                    {ok, State#recvr_state{statestore = StateStore}};
-                Error -> Error
-            end;
-        {error, Reason} ->
-            {error, Reason}
-    end.
+    {ok, State#recvr_state{statestore = undefined}}.
+
+    %% StateFile = string:concat(integer_to_list(Partition), "replstate"),
+    %% Path = filename:join(
+    %%          app_helper:get_env(riak_core, platform_data_dir), StateFile),
+    %% case dets:open_file(StateFile, [{file, Path}, {type, set}]) of
+    %%     {ok, StateStore} ->
+    %%         case dets:lookup(StateStore, recvr_state) of
+    %%             %%If file already exists read previous state from it.
+    %%             [{recvr_state, State}] ->
+    %%                 {ok, State};
+    %%             [] ->
+    %%                 {ok, State } = inter_dc_repl_update:init_state(Partition),
+    %%                 {ok, State#recvr_state{statestore = StateStore}};
+    %%             Error -> Error
+    %%         end;
+    %%     {error, Reason} ->
+    %%         {error, Reason}
+    %% end.
 
 %% process one replication request from other Dc. Update is put in a queue for each DC.
 %% Updates are expected to recieve in causal order.
