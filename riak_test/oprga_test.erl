09de5e613f45a78a91c66d81d88cd743b0da5cc6
Santiago Alvarez <santialvarezcolombo@remoteaccess-wism-guest-ipv4-0005.misc.uni-kl.de>
2015-09-07 15:54:19 +0200

    Added value method to RGA and fixed tests

--- riak_test/oprga_test.erl
+++ riak_test/oprga_test.erl
@@ -9,13 +9,37 @@ confirm() ->
     [Nodes] = rt:build_clusters([3]),
     lager:info("Nodes: ~p", [Nodes]),
     empty_test(Nodes),
+    add_test(Nodes),
     rt:clean_cluster(Nodes),
     pass.
 
 empty_test(Nodes) ->
     FirstNode = hd(Nodes),
-    lager:info("empty_test started"),
+    lager:info("Empty test started"),
     Type = crdt_rga,
     Key = key_empty,
     Result0 = rpc:call(FirstNode, antidote, read, [Key, Type]),
-    ?assertMatch({ok, []}, Result0).
+    ?assertMatch({ok, []}, Result0),
+    lager:info("Empty test OK").
+
+add_test(Nodes) ->
+    FirstNode = hd(Nodes),
+    lager:info("Add test started"),
+    Type = crdt_rga,
+    Key = key_add,
+    Result0 = rpc:call(FirstNode, antidote, clocksi_execute_tx,
+        [[{update, Key, Type, {{addRight, a, 1}, ucl}}]]),
+    ?assertMatch({ok, _}, Result0),
+    Result1 = rpc:call(FirstNode, antidote, read, [Key, Type]),
+    ?assertMatch({ok, [{ok, a, _}]}, Result1),
+    Result2 = rpc:call(FirstNode, antidote, clocksi_execute_tx,
+        [[{update, Key, Type, {{addRight, b, 1}, ucl}}]]),
+    ?assertMatch({ok, _}, Result2),
+    Result3 = rpc:call(FirstNode, antidote, read, [Key, Type]),
+    ?assertMatch({ok, [{ok, a, _}, {ok, b, _}]}, Result3),
+    Result4 = rpc:call(FirstNode, antidote, clocksi_execute_tx,
+        [[{update, Key, Type, {{addRight, c, 2}, ucl}}]]),
+    ?assertMatch({ok, _}, Result4),
+    Result5 = rpc:call(FirstNode, antidote, read, [Key, Type]),
+    ?assertMatch({ok, [{ok, a, _}, {ok, b, _}, {ok, c, _}]}, Result5),
+    lager:info("Add test OK").
