f9194058e0fc0208f51dffe33e46b309e20142de
Michał Jabczyński <michal@jabczynski.net>
2015-09-14 19:42:53 +0200

    Last log opid passed along with interdc txns to eliminate gaps

--- include/inter_dc_repl.hrl
+++ include/inter_dc_repl.hrl
@@ -11,11 +11,12 @@
 -type zmq_socket() :: any().
 -type pdcid() :: {dcid(), partition_id()}.
 -type interdc_descriptor() :: {dcid(), [socket_address()], [socket_address()]}.
+-type log_opid() :: non_neg_integer().
 
 -record(interdc_txn, {
  dcid :: dcid(),
  partition :: partition_id(),
- logid_range :: {non_neg_integer(), non_neg_integer()},
+ prev_log_opid :: log_opid() | none, %% the value is *none* if the transaction is read directly from the log
  snapshot :: snapshot_time(),
  timestamp :: non_neg_integer(),
  operations :: [operation()] %% if the OP list is empty, the message is a HEARTBEAT
