b5e659a7a9e4dd59b648255c0f4f4f7b4fb1f6e6
tcrain <tyler.crain@lip6.fr>
2015-09-28 18:46:56 +0200

    disalbe sync logging

--- include/antidote.hrl
+++ include/antidote.hrl
@@ -37,7 +37,7 @@
 -define(SPIN_WAIT, 10).
 %% At commit, if this is set to true, the logging vnode
 %% will ensure that the transaction record is written to disk
--define(SYNC_LOG, true).
+-define(SYNC_LOG, false).
 -record (payload, {key:: key(), type :: type(), op_param, actor}).
 %% Enalble or disable the certification check
 -ifdef(NO_CERTIFICATION).
