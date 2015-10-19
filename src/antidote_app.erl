8df018284f3a5f70b064181a8a8489d7a13fda4d
tcrain <tyler.crain@lip6.fr>
2015-10-19 18:35:05 +0200

    reverting to old pb interface

--- src/antidote_app.erl
+++ src/antidote_app.erl
@@ -25,7 +25,10 @@
 -export([start/2, stop/1]).
 
 %% PB Services
--define(SERVICES, [{antidote_pb_txn, 109, 128}]).
+%-define(SERVICES, [{antidote_pb_txn, 109, 128}]).
+-define(SERVICES, [{antidote_pb_counter, 94, 98},
+                   {antidote_pb_set, 99, 101},		
+                   {antidote_pb_txn, 102, 108}]).
 
 %% ===================================================================
 %% Application callbacks
@@ -34,7 +37,6 @@
 start(_StartType, _StartArgs) ->
     case antidote_sup:start_link() of
         {ok, Pid} ->
-	    lager:info("Starting antidote app"),
             ok = riak_core:register([{vnode_module, logging_vnode}]),
             ok = riak_core_node_watcher:service_up(logging, self()),
             %%ClockSI layer
@@ -45,10 +47,14 @@ start(_StartType, _StartArgs) ->
             ok = riak_core:register([{vnode_module, materializer_vnode}]),
             ok = riak_core_node_watcher:service_up(materializer, self()),
 
-            ok = riak_core:register([{vnode_module, inter_dc_repl_vnode}]),
-            ok = riak_core_node_watcher:service_up(interdcreplication, self()),
-            ok = riak_core:register([{vnode_module, inter_dc_recvr_vnode}]),
-            ok = riak_core_node_watcher:service_up(inter_dc_recvr, self()),
+            ok = riak_core:register([{vnode_module, inter_dc_log_sender_vnode}]),
+            ok = riak_core_node_watcher:service_up(logsender, self()),
+
+            ok = riak_core:register([{vnode_module, inter_dc_sub_vnode}]),
+            ok = riak_core_node_watcher:service_up(inter_dc_sub, self()),
+
+            ok = riak_core:register([{vnode_module, inter_dc_dep_vnode}]),
+            ok = riak_core_node_watcher:service_up(inter_dc_dep, self()),
 
             ok = riak_core_ring_events:add_guarded_handler(antidote_ring_event_handler, []),
             ok = riak_core_node_watcher_events:add_guarded_handler(antidote_node_event_handler, []),
--- src/antidote_pb_txn.erl
+++ src/antidote_pb_txn.erl
@@ -47,144 +47,116 @@ init() ->
 decode(Code, Bin) ->
     Msg = riak_pb_codec:decode(Code, Bin),
     case Msg of
-        #apbstarttransaction{} ->
-            {ok, Msg, {"antidote.startxn",<<>>}};
-        #apbaborttransaction{} ->
-            {ok, Msg, {"antidote.aborttxn",<<>>}};
-        #apbcommittransaction{} ->
-            {ok, Msg, {"antidote.committxn",<<>>}};
-        #apbreadobjects{} ->
-            {ok, Msg, {"antidote.readobjects",<<>>}};
-        #apbupdateobjects{} ->
-            {ok, Msg, {"antidote.updateobjects",<<>>}};
-        #apbstaticupdateobjects{} ->
-            {ok, Msg, {"antidote.staticupdateobjects",<<>>}};
-        #apbstaticreadobjects{} ->
-            {ok, Msg, {"antidote.staticreadobjects",<<>>}}
+        #fpbatomicupdatetxnreq{} ->
+            {ok, Msg, {"antidote.atomicupdate", <<>>}};
+        #fpbsnapshotreadtxnreq{} ->
+            {ok, Msg, {"antidote.snapshotread",<<>>}}
     end.
 
 %% @doc encode/1 callback. Encodes an outgoing response message.
 encode(Message) ->
     {ok, riak_pb_codec:encode(Message)}.
 
-process(#apbstarttransaction{timestamp=BClock, properties = BProperties},
-        State) ->
+%% @doc process/2 callback. Handles an incoming request message.
+process(#fpbatomicupdatetxnreq{clock=BClock,ops = Ops}, State) ->
+    Updates = decode_au_txn_ops(Ops),
     Clock = binary_to_term(BClock),
-    Properties = antidote_pb_codec:decode(txn_properties, BProperties),
-    Response = antidote:start_transaction(Clock, Properties),
+    Response =
+        case Clock of
+            ignore ->
+                antidote:clocksi_bulk_update(Updates);
+            _ ->
+                antidote:clocksi_bulk_update(Clock, Updates)
+        end,
     case Response of
-        {ok, TxId} ->
-            {reply, antidote_pb_codec:encode(start_transaction_response,
-                                             {ok, TxId}),
-             State};
-        {error, Reason} ->
-            {reply, antidote_pb_codec:encode(start_transaction_response,
-                                             {error, Reason}),
+        {error, _Reason} ->
+            {reply, #fpbatomicupdatetxnresp{success = false}, State};
+        {ok, {_Txid, _ReadSet, CommitTime}} ->
+            {reply, #fpbatomicupdatetxnresp{success = true,
+                                            clock=term_to_binary(CommitTime)},
              State}
     end;
 
-process(#apbaborttransaction{transaction_descriptor=Td}, State) ->
-    TxId = binary_to_term(Td),
-    Response = antidote:abort_transaction(TxId),
-    case Response of
-        {error, Reason} ->
-            {reply, antidote_pb_codec:encode(operation_response,
-                                             {error, Reason}),
-             State}
-            %% TODO: client initiated abort is not implemented yet
-            %% Add the following only after it is implemented to avoid dialyzer errors
-            %% ok ->
-            %%     {reply, antidote_pb_codec:encode(operation_response, ok),
-            %%      State}
-    end;
-
-process(#apbcommittransaction{transaction_descriptor=Td}, State) ->
-    TxId = binary_to_term(Td),
-    Response = antidote:commit_transaction(TxId),
-    case Response of
-        {error, Reason} ->
-            {reply, antidote_pb_codec:encode(commit_response,
-                                             {error, Reason}), State};
-        {ok, CommitTime} ->
-            {reply, antidote_pb_codec:encode(commit_response, {ok, CommitTime}),
-             State}
-    end;
-
-process(#apbreadobjects{boundobjects=BoundObjects, transaction_descriptor=Td},
-        State) ->
-    Objects = lists:map(fun(O) ->
-                                antidote_pb_codec:decode(bound_object, O) end,
-                        BoundObjects),
-
-    TxId = binary_to_term(Td),
-    Response = antidote:read_objects(Objects, TxId),
-    case Response of
-        {error, Reason} ->
-            {reply, antidote_pb_codec:encode(read_objects_response,
-                                             {error, Reason}), State};
-        {ok, Results} ->
-            {reply, antidote_pb_codec:encode(read_objects_response,
-                                             {ok, lists:zip(Objects,Results)}),
-             State}
-    end;
-
-process(#apbupdateobjects{updates=BUpdates, transaction_descriptor=Td},
-        State) ->
-    Updates = lists:map(fun(O) ->
-                                antidote_pb_codec:decode(update_object, O) end,
-                        BUpdates),
-
-    TxId = binary_to_term(Td),
-    Response = antidote:update_objects(Updates, TxId),
-    case Response of
-        {error, Reason} ->
-            {reply, antidote_pb_codec:encode(operation_response,
-                                             {error, Reason}),
-             State};
-        ok ->
-            {reply, antidote_pb_codec:encode(operation_response, ok),
-             State}
-    end;
-process(#apbstaticupdateobjects{
-           transaction=#apbstarttransaction{timestamp=BClock, properties = BProperties},
-           updates=BUpdates},
-        State) ->
-
+process(#fpbsnapshotreadtxnreq{clock=BClock,ops = Ops}, State) ->
+    ReadReqs = decode_snapshot_read_ops(Ops),
+    %%TODO: change this to interactive reads
     Clock = binary_to_term(BClock),
-    Properties = antidote_pb_codec:decode(txn_properties, BProperties),
-    Updates = lists:map(fun(O) ->
-                                antidote_pb_codec:decode(update_object, O) end,
-                        BUpdates),
-    Response = antidote:update_objects(Clock, Properties, Updates),
+    Response =
+        case Clock of
+            ignore ->
+                antidote:clocksi_execute_tx(ReadReqs);
+            _ -> antidote:clocksi_execute_tx(Clock, ReadReqs)
+        end,
     case Response of
-        {error, Reason} ->
-            {reply, antidote_pb_codec:encode(commit_response,
-                                             {error, Reason}), State};
-        {ok, CommitTime} ->
-            {reply, antidote_pb_codec:encode(commit_response, {ok, CommitTime}),
-             State}
-    end;            
-process(#apbstaticreadobjects{
-           transaction=#apbstarttransaction{timestamp=BClock, properties = BProperties},
-           objects=BoundObjects},
-        State) ->
-    Clock = binary_to_term(BClock),
-    Properties = antidote_pb_codec:decode(txn_properties, BProperties),
-    Objects = lists:map(fun(O) ->
-                                antidote_pb_codec:decode(bound_object, O) end,
-                        BoundObjects),
-    Response = antidote:read_objects(Clock, Properties, Objects),
-    case Response of
-        {error, Reason} ->
-            {reply, antidote_pb_codec:encode(commit_response,
-                                             {error, Reason}), State};
-        {ok, Results, CommitTime} ->
-            {reply, antidote_pb_codec:encode(static_read_objects_response,
-                                             {ok, lists:zip(Objects,Results), CommitTime}),
-             State}
+        {ok, {_TxId, ReadSet, CommitTime}} ->
+            Zipped = lists:zip(ReadReqs, ReadSet),
+            Reply = encode_snapshot_read_response(Zipped),
+            {reply, #fpbsnapshotreadtxnresp{success=true,
+                                            clock= term_to_binary(CommitTime),
+                                            results=Reply}, State};
+        Other ->
+            lager:info("Clocksi execute received ~p",[Other]),
+            {reply, #fpbsnapshotreadtxnresp{success=false}, State}
     end.
+
 
 %% @doc process_stream/3 callback. This service does not create any
 %% streaming responses and so ignores all incoming messages.
 process_stream(_,_,State) ->
     {ignore, State}.
+
+%% @doc decode_au_txn_ops : converts the pb messages for atomic update
+%% transaction into update messages for interface in antidote.erl
+-spec decode_au_txn_ops([#fpbatomicupdatetxnop{}]) ->
+                               [{update, key(), type(), op()}].
+decode_au_txn_ops(Ops) ->
+    lists:foldl(fun(Op, Acc) ->
+                     Acc ++ decode_au_txn_op(Op)
+                end, [], Ops).
+%% Counter
+decode_au_txn_op(#fpbatomicupdatetxnop{counterinc=#fpbincrementreq{key=Key, amount=Amount}}) ->
+    [{update, Key, riak_dt_pncounter, {{increment, Amount}, node()}}];
+decode_au_txn_op(#fpbatomicupdatetxnop{counterdec=#fpbdecrementreq{key=Key, amount=Amount}}) ->
+    [{update, Key, riak_dt_pncounter, {{decrement, Amount}, node()}}];
+%% Set
+decode_au_txn_op(#fpbatomicupdatetxnop{setupdate=#fpbsetupdatereq{key=Key,adds=AddElems, rems=RemElems}}) ->
+     Adds = lists:map(fun(X) ->
+                              binary_to_term(X)
+                      end, AddElems),
+     Rems = lists:map(fun(X) ->
+                              binary_to_term(X)
+                      end, RemElems),
+    Op = case length(Adds) of
+             0 -> [];
+             1 -> [{update, Key, riak_dt_orset, {{add,Adds}, node()}}];
+             _ -> [{update, Key, riak_dt_orset, {{add_all, Adds},node()}}]
+         end,
+    case length(Rems) of
+        0 -> Op;
+        1 -> [{update, Key, riak_dt_orset, {{remove, hd(Rems)}, ignore}}] ++ Op;
+        _ -> [{update, Key, riak_dt_orset, {{remove_all, Rems},ignore}}] ++ Op
+    end.
+
+%% @doc decode_snapshot_read_ops : converts the pb messages for snapshot
+%%  read transaction into read messages.
+-spec decode_snapshot_read_ops([#fpbsnapshotreadtxnop{}]) ->
+                                      [{read, key(), type()}].
+decode_snapshot_read_ops(Ops) ->
+    lists:map(fun(Op) ->
+                      decode_snapshot_read_op(Op)
+              end, Ops).
+
+decode_snapshot_read_op(#fpbsnapshotreadtxnop{counter=#fpbgetcounterreq{key=Key}}) ->
+    {read, Key, riak_dt_pncounter};
+decode_snapshot_read_op(#fpbsnapshotreadtxnop{set=#fpbgetsetreq{key=Key}}) ->
+    {read,Key, riak_dt_orset}.
+
+%% @doc encodes the response for snapshot read into pb messages
+encode_snapshot_read_response(Zipped) ->
+    lists:map(fun(Resp) ->
+                      encode_snapshot_read_resp(Resp)
+              end, Zipped).
+encode_snapshot_read_resp({{read, Key, riak_dt_pncounter}, Result}) ->
+    #fpbsnapshotreadtxnrespvalue{key=Key,counter=#fpbgetcounterresp{value =Result}};
+encode_snapshot_read_resp({{read,Key,riak_dt_orset}, Result}) ->
+    #fpbsnapshotreadtxnrespvalue{key=Key,set=#fpbgetsetresp{value = term_to_binary(Result)}}.
