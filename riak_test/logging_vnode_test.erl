794e795215ecb2b4dfb4a70144e4c4c2f9550b0d
santialvarezcolombo <santialvarezcolombo@gmail.com>
2015-10-30 17:26:43 +0100

    fixed tests

--- riak_test/logging_vnode_test.erl
+++ riak_test/logging_vnode_test.erl
@@ -3,6 +3,7 @@
 -export([confirm/0]).
 
 -include_lib("eunit/include/eunit.hrl").
+-include("antidote.hrl").
 -define(HARNESS, (rt_config:get(rt_harness))).
 
 confirm() ->
@@ -19,25 +20,51 @@ confirm() ->
 
     lager:info("Nodes: ~p", [Nodes]),
     read_initial_value_not_in_ets_test(Nodes),
+    read_old_value_not_in_ets_test(Nodes),
     pass.
 
-%% First we remember the initial time of the counter (value 0).
-%% After 10 updates, the smallest snapshot in ets table is 5,
-%% but a read of the previous snapshot returns 0, fetching the value from the log.
+%% First we remember the initial time of the counter (with value 0).
+%% After 15 updates, the smallest snapshot in ets table is 10,
+%% but if we read the initial value of the counter,
+%% we get 0, because no operations have a smaller commit time in the
+%% reconstruction of time 0 in the log. (this is a corner case)
 read_initial_value_not_in_ets_test(Nodes) ->
     lager:info("read_snapshot_not_in_ets_test started"),
     FirstNode = hd(Nodes),
     Type = crdt_pncounter,
-    Key = empty_test,
+    Key = initial_value_test,
     {ok, TxId} = rpc:call(FirstNode, antidote, clocksi_istart_tx, []),
-    ReadResult = rpc:call(FirstNode, antidote, clocksi_iread,
-        [TxId, Key, Type]),
-    ?assertEqual({ok, 0}, ReadResult),
-    increment_counter(FirstNode, Key, 11),
-    ReadResult1 = rpc:call(FirstNode,
+    increment_counter(FirstNode, Key, 15),
+    %% old read value is 0
+    {ok, ReadResult1} = rpc:call(FirstNode,
         antidote, clocksi_iread, [TxId, Key, Type]),
-    ?assertEqual({ok, 0}, ReadResult1),
+    ?assertEqual(0, ReadResult1),
+    %% most recent read value is 15
+    {ok, {_, [ReadResult2], _}} = rpc:call(FirstNode,
+        antidote, clocksi_read, [Key, Type]),
+    ?assertEqual(15, ReadResult2),
     lager:info("read_snapshot_not_in_ets_test OK").
+
+read_old_value_not_in_ets_test(Nodes) ->
+    lager:info("read_old_value_not_in_ets_test started"),
+    FirstNode = hd(Nodes),
+    Type = crdt_pncounter,
+    Key = read_old_val,
+    increment_counter(FirstNode, Key, 5),
+    {ok, {_, [ReadResult], _}} = rpc:call(FirstNode, antidote, clocksi_execute_tx,
+        [[{read, {Key, Type}}]]),
+    ?assertEqual(5, ReadResult),
+    {ok, TxId} = rpc:call(FirstNode, antidote, clocksi_istart_tx, []),
+    increment_counter(FirstNode, Key, 15),
+    %% old read value is 5
+    {ok, ReadResult1} = rpc:call(FirstNode,
+        antidote, clocksi_iread, [TxId, Key, Type]),
+    ?assertEqual(5, ReadResult1),
+    %% most recent read value is 20
+    {ok, {_, [ReadResult2], _}} = rpc:call(FirstNode, antidote, clocksi_execute_tx,
+        [[{read, {Key, Type}}]]),
+    ?assertEqual(20, ReadResult2),
+    lager:info("read_old_value_not_in_ets_test OK").
 
 %% Auxiliary method to increment a counter N times.
 increment_counter(_FirstNode, _Key, 0) ->
