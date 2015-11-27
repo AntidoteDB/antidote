bd6e3f5464dcd53e71bff5c5d7370960f9237697
tcrain <tyler.crain@lip6.fr>
2015-11-27 18:40:42 +0100

    a clean version of weak meta data with pubsub

--- src/antidote_app.erl
+++ src/antidote_app.erl
@@ -41,9 +41,6 @@ start(_StartType, _StartArgs) ->
             ok = riak_core:register([{vnode_module, clocksi_vnode}]),
             ok = riak_core_node_watcher:service_up(clocksi, self()),
 
-            ok = riak_core:register([{vnode_module, vectorclock_vnode}]),
-            ok = riak_core_node_watcher:service_up(vectorclock, self()),
-
             ok = riak_core:register([{vnode_module, materializer_vnode}]),
             ok = riak_core_node_watcher:service_up(materializer, self()),
 
