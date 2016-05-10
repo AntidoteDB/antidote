a1ab3123778e5971afa0742d9ca08d435a608e16
deepthidevaki <deepthidevaki@gmail.com>
2016-05-10 10:50:45 +0200

    modified orset to antidote_crdt behaviour

--- src/antidote_crdt_orset.erl
+++ src/antidote_crdt_orset.erl
@@ -23,7 +23,7 @@
 %% @doc
 %% An operation-based Observed-Remove Set CRDT.
 %% As the data structure is operation-based, to issue an operation, one should
-%% firstly call `generate_downstream/3' to get the downstream version of the 
+%% firstly call `generate_downstream/3' to get the downstream version of the
 %% operation and then call `update/2'.
 %%
 %% It provides five operations: add, which adds an element to a set; add_all,
@@ -31,7 +31,7 @@
 %% remove_all that removes a list of elements from the set; update, that contains
 %% a list of previous four commands.
 %%
-%% This file is adapted from riak_dt_orset, a state-based implementation of 
+%% This file is adapted from riak_dt_orset, a state-based implementation of
 %% Observed-Remove Set.
 %% The changes are as follows:
 %% 1. `generate_downstream/3' is added, as this is necessary for op-based CRDTs.
@@ -42,11 +42,29 @@
 %% Convergent and Commutative Replicated Data Types. http://hal.upmc.fr/inria-00555588/
 %%
 %% @end
--module(crdt_orset).
+-module(antidote_crdt_orset).
 
-%% API
--export([new/0, value/1, generate_downstream/3, update/2, equal/2,
-    to_binary/1, from_binary/1, value/2, precondition_context/1, stats/1, stat/2, is_operation/1]).
+-include("antidote_crdt.hrl").
+
+%% Callbacks
+-export([ new/0,
+          value/1,
+          value/2,
+          downstream/2,
+          update/2,
+          equal/2,
+          to_binary/1,
+          from_binary/1,
+          is_operation/1,
+          require_state_downstream/1
+        ]).
+
+%% Others
+-export([ precondition_context/1,
+          stats/1
+        ]).
+
+-behaviour(antidote_crdt).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -58,20 +76,18 @@
 -type binary_orset() :: binary(). %% A binary that from_binary/1 will operate on.
 
 -type orset_op() :: {add, member()} | {remove, member()} |
-                    {add_all, [member()]} | {remove_all, [member()]} |
-                    {update, [orset_op()]}.
+                    {add_all, [member()]} | {remove_all, [member()]}.
 
--type actor() :: riak_dt:actor().
 -type member() :: term().
 
 -spec new() -> orset().
 new() ->
     orddict:new().
 
-%% @doc   
-%% without parameter: return all existing elements in the `orset()'  
-%% {fragment, elem}: create and return a new `orset()' with all metadata 
-%% of an element 
+%% @doc
+%% without parameter: return all existing elements in the `orset()'
+%% {fragment, elem}: create and return a new `orset()' with all metadata
+%% of an element
 %% {tokens, elem}: returns all uniques tokens of an element in the set
 -spec value(orset()) -> [member()].
 value(ORDict) ->
@@ -95,27 +111,28 @@ value({tokens, Elem}, ORSet) ->
 value(_,ORSet) ->
     value(ORSet).
 
-%% @doc generate downstream operations. 
+%% @doc generate downstream operations.
 %% If the operation is add or add_all, generate unique tokens for each element
-%% If the operation is remove or remove_all, fetches all unique tokens for 
+%% If the operation is remove or remove_all, fetches all unique tokens for
 %% these elements existing in the `orset()'.
--spec generate_downstream(orset_op(), actor(), orset()) -> {ok, orset_op()}.
-generate_downstream({add,Elem}, Actor, _ORDict) ->
-    Token = unique(Actor),
+-spec downstream(orset_op(), orset()) -> {ok, orset_op()}.
+downstream({add,Elem}, _ORDict) ->
+    Token = unique(),
     {ok, {add, {Elem,[Token]}}};
-generate_downstream({add_all,Elems}, Actor, _ORDict0) ->
+downstream({add_all,Elems}, _ORDict0) ->
     DownstreamOp = lists:foldl(fun(Elem, Sum) ->
-                                Token = unique(Actor),
-                                Sum++[{Elem, [Token]}]
-                                end, [], Elems),
+                                       Token = unique(),
+                                       Sum++[{Elem, [Token]}]
+                               end, [], Elems),
     {ok, {add_all, DownstreamOp}};
-generate_downstream({remove, Elem}, _Actor, ORDict) ->
+downstream({remove, Elem}, ORDict) ->
     ToRemove = value({tokens, Elem}, ORDict),
     {ok, {remove, {Elem, ToRemove}}};
-generate_downstream({remove_all,Elems}, _Actor, ORDict) ->
-    ToRemove = lists:foldl(fun(Elem, Sum) -> Sum++[{Elem, value({tokens, Elem}, ORDict)}] end, [], Elems),
+downstream({remove_all,Elems}, ORDict) ->
+    ToRemove = lists:foldl(fun(Elem, Sum) ->
+                                   Sum++[{Elem, value({tokens, Elem}, ORDict)}]
+                           end, [], Elems),
     {ok, {remove_all, ToRemove}}.
-
 
 %% @doc apply downstream operations and update an `orset()'.
 %% The first parameter denotes this operation is for adding or removing elements.
@@ -123,24 +140,22 @@ generate_downstream({remove_all,Elems}, _Actor, ORDict) ->
 %% For remove or remove_all, the second element of the tuple is a list of elements and their tokens
 %% to remove.
 %% For update, the second element of the tuple is a list of updates to apply, each of which can
-%% either be add, add_all or remove, remove_all. 
--spec update(orset_op(), orset()) -> {ok, orset()} |
-                                              {error, {precondition ,{not_present, member()}}}.
+%% either be add, add_all or remove, remove_all.
+-spec update(orset_op(), orset()) ->
+                    {ok, orset()} |
+                    {error, {precondition ,{not_present, member()}}}.
 update({add, {Elem, [Token|_]}}, ORDict) ->
     add_elem(Elem,Token,ORDict);
 update({add_all,Elems}, ORDict0) ->
     OD = lists:foldl(fun(Elem,ORDict) ->
-                {ok, ORDict1} = update({add,Elem},ORDict),
-                ORDict1
-            end, ORDict0, Elems),
+                             {ok, ORDict1} = update({add,Elem},ORDict),
+                             ORDict1
+                     end, ORDict0, Elems),
     {ok, OD};
 update({remove, Elem}, ORDict) ->
     remove_elem(Elem, ORDict);
 update({remove_all,Elems}, ORDict0) ->
-    remove_elems(Elems, ORDict0);
-update({update, Ops}, ORDict) ->
-    apply_ops(Ops, ORDict).
-
+    remove_elems(Elems, ORDict0).
 
 -spec equal(orset(), orset()) -> boolean().
 equal(ORDictA, ORDictB) ->
@@ -204,14 +219,14 @@ remove_elem({Elem,RemoveTokens},ORDict) ->
     case orddict:find(Elem,ORDict) of
         {ok, Tokens} ->
             RestTokens = Tokens--RemoveTokens,
-            case RestTokens of 
+            case RestTokens of
                 [] ->
                     {ok, orddict:erase(Elem, ORDict)};
-                _ -> 
+                _ ->
                     {ok, orddict:store(Elem, Tokens--RemoveTokens, ORDict)}
             end;
         error ->
-            case RemoveTokens of 
+            case RemoveTokens of
                 [] ->
                     {ok, ORDict};
                 _ ->
@@ -227,16 +242,8 @@ remove_elems([Elem|Rest], ORDict) ->
         Error         -> Error
     end.
 
-apply_ops([], ORDict) ->
-    {ok, ORDict};
-apply_ops([Op | Rest], ORDict) ->
-    case update(Op, ORDict) of
-        {ok, ORDict1} -> apply_ops(Rest, ORDict1);
-        Error -> Error
-    end.
-
 %% @doc generate a unique identifier (best-effort).
-unique(_Actor) ->
+unique() ->
     crypto:strong_rand_bytes(20).
 
 minimum_tokens(Tokens) ->
@@ -247,21 +254,31 @@ minimum_tokens(Tokens) ->
 %% @doc The following operation verifies
 %%      that Operation is supported by this particular CRDT.
 -spec is_operation(term()) -> boolean().
-is_operation(Operation) ->
-    riak_dt_orset:is_operation(Operation).
+is_operation({add, _Elem}) -> true;
+is_operation({add_all, [_H|_T]}) -> true;
+is_operation({remove, _Elem}) ->
+    true;
+is_operation({remove_all, [_H|_T]}) -> true.
+
+require_state_downstream({add,_}) ->
+    false;
+require_state_downstream({add_all,_}) ->
+    false;
+require_state_downstream({remove,_}) -> true;
+require_state_downstream({remove_all, _}) -> true.
 
 %% ===================================================================
 %% EUnit tests
 %% ===================================================================
 -ifdef(TEST).
 new_test() ->
-    ?assertEqual(orddict:new(), new()). 
+    ?assertEqual(orddict:new(), new()).
 
 add_test() ->
     Set1 = new(),
-    {ok, DownstreamOp1} = generate_downstream({add, <<"foo">>}, 1, Set1),
+    {ok, DownstreamOp1} = downstream({add, <<"foo">>}, Set1),
     ?assertMatch({add, {<<"foo">>, _}}, DownstreamOp1),
-    {ok, DownstreamOp2} = generate_downstream({add_all, [<<"li">>,<<"manu">>]}, 1, Set1),
+    {ok, DownstreamOp2} = downstream({add_all, [<<"li">>,<<"manu">>]}, Set1),
     ?assertMatch({add_all, [{<<"li">>, _}, {<<"manu">>, _}]}, DownstreamOp2),
     {ok, Set2} = update(DownstreamOp1, Set1),
     {_, Elem1} = DownstreamOp1,
@@ -272,11 +289,11 @@ add_test() ->
 
 value_test() ->
     Set1 = new(),
-    {ok, DownstreamOp1} = generate_downstream({add, <<"foo">>}, 1, Set1),
+    {ok, DownstreamOp1} = downstream({add, <<"foo">>}, Set1),
     ?assertEqual([], value(Set1)),
     {ok, Set2} = update(DownstreamOp1, Set1),
     ?assertEqual([<<"foo">>], value(Set2)),
-    {ok, DownstreamOp2} = generate_downstream({add_all, [<<"foo">>, <<"li">>,<<"manu">>]}, 1, Set2),
+    {ok, DownstreamOp2} = downstream({add_all, [<<"foo">>, <<"li">>,<<"manu">>]}, Set2),
     {ok, Set3} = update(DownstreamOp2, Set2),
     ?assertEqual([<<"foo">>, <<"li">>, <<"manu">>], value(Set3)),
 
@@ -290,41 +307,41 @@ value_test() ->
 remove_test() ->
     Set1 = new(),
     %% Add an element then remove it
-    {ok, Op1} = generate_downstream({add, <<"foo">>}, 1, Set1),
+    {ok, Op1} = downstream({add, <<"foo">>}, 1, Set1),
     {ok, Set2} = update(Op1, Set1),
     ?assertEqual([<<"foo">>], value(Set2)),
-    {ok, Op2} = generate_downstream({remove, <<"foo">>}, 1, Set2),
+    {ok, Op2} = downstream({remove, <<"foo">>}, 1, Set2),
     {ok, Set3} = update(Op2, Set2),
     ?assertEqual([], value(Set3)),
 
     %% Add many elements then remove part
-    {ok, Op3} = generate_downstream({add_all, [<<"foo">>, <<"li">>,<<"manu">>]}, 1, Set1),
+    {ok, Op3} = downstream({add_all, [<<"foo">>, <<"li">>,<<"manu">>]}, Set1),
     {ok, Set4} = update(Op3, Set1),
     ?assertEqual([<<"foo">>, <<"li">>, <<"manu">>], value(Set4)),
 
-    {ok, Op5} = generate_downstream({remove_all, [<<"foo">>, <<"li">>]},1 , Set4),
+    {ok, Op5} = downstream({remove_all, [<<"foo">>, <<"li">>]}, Set4),
     {ok, Set5} = update(Op5, Set4),
     ?assertEqual([<<"manu">>], value(Set5)),
-    
+
     %% Remove more than current have
-    {ok, Op6} = generate_downstream({add_all, [<<"foo">>, <<"li">>,<<"manu">>]}, 1, Set1),
+    {ok, Op6} = downstream({add_all, [<<"foo">>, <<"li">>,<<"manu">>]}, Set1),
     {ok, Set6} = update(Op6, Set1),
-    {ok, Op7} = generate_downstream({remove_all, [<<"manu">>, <<"test">>]}, 1, Set6),
+    {ok, Op7} = downstream({remove_all, [<<"manu">>, <<"test">>]}, Set6),
     Result = update(Op7, Set6),
     ?assertMatch({ok, _}, Result).
 
-    
+
 concurrent_add_test() ->
     Set1 = new(),
     %% Add an element then remove it
-    {ok, Op1} = generate_downstream({add, <<"foo">>}, 1, Set1),
+    {ok, Op1} = downstream({add, <<"foo">>}, Set1),
     {ok, Set2} = update(Op1, Set1),
     ?assertEqual([<<"foo">>], value(Set2)),
-    
-    %% If remove is concurrent with the second add, will not remove the second added 
-    {ok, Op2} = generate_downstream({remove, <<"foo">>}, 1, Set2),
 
-    {ok, Op3} = generate_downstream({add, <<"foo">>}, 1, Set1),
+    %% If remove is concurrent with the second add, will not remove the second added
+    {ok, Op2} = downstream({remove, <<"foo">>}, Set2),
+
+    {ok, Op3} = downstream({add, <<"foo">>}, Set1),
     {ok, Set3} = update(Op3, Set2),
     ?assertEqual([<<"foo">>], value(Set3)),
 
@@ -332,17 +349,17 @@ concurrent_add_test() ->
     ?assertEqual([<<"foo">>], value(Set4)),
 
     %% If remove follows two adds, remove will remove all
-    {ok, Op4} = generate_downstream({remove, <<"foo">>}, 1, Set3),
+    {ok, Op4} = downstream({remove, <<"foo">>}, Set3),
     {ok, Set5} = update(Op4, Set3),
     ?assertEqual([], value(Set5)).
-    
+
 binary_test() ->
     ORSet1 = new(),
     BinaryORSet1 = to_binary(ORSet1),
     ORSet2 = from_binary(BinaryORSet1),
     ?assert(equal(ORSet1, ORSet2)),
 
-    {ok, Op1} = generate_downstream({add, <<"foo">>}, 1, ORSet1),
+    {ok, Op1} = downstream({add, <<"foo">>}, ORSet1),
     {ok, ORSet3} = update(Op1, ORSet1),
     BinaryORSet3 = to_binary(ORSet3),
     ORSet4 = from_binary(BinaryORSet3),
