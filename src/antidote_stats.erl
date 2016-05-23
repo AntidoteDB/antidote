7a16a58065b698549b6680122b39f6ba12aa9eea
deepthidevaki <deepthidevaki@gmail.com>
2016-05-23 11:35:31 +0200

    collect exometer metrics

---
+++ src/antidote_stats.erl
@@ -0,0 +1,56 @@
+-module(antidote_stats).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0,  get_value/2, stats/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+%% List of configured metrics
+stats() ->
+    [[staleness], [random]].
+
+get_value(_Pid, Metric) ->    
+    gen_server:call(?MODULE, {get_value, Metric}).
+
+init([]) ->
+        {ok, ok}.
+
+handle_call({get_value, Metric}, _From, State) ->
+    {reply, calculate(Metric), State};
+
+handle_call(_Req, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast(_Req, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+calculate([staleness]) ->
+    {ok, SS} = vectorclock:get_stable_snapshot(),
+    CurrentClock = to_microsec(os:timestamp()),
+    Staleness = dict:fold(fun(_K, C, Max) ->
+                                   max(CurrentClock - C, Max)
+                           end, 0, SS),
+    Staleness;
+
+calculate([random]) ->
+    random:uniform(100).
+
+to_microsec({MegaSecs, Secs, MicroSecs}) ->
+    (MegaSecs * 1000000 + Secs) * 1000000 + MicroSecs.
+    
---
+++ src/antidote_stats_collector.erl
@@ -0,0 +1,54 @@
+-module(antidote_stats_collector).
+
+-behaviour(gen_server).
+-define(INTERVAL, 10000). %% 10 sec
+-define(INIT_INTERVAL, 60000). %% 1 minute
+
+%% API
+-export([start_link/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+start_link(StatsPid) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [StatsPid], []).
+
+init([StatsPid]) ->
+    init_metrics(),
+    Timer = erlang:send_after(?INIT_INTERVAL, self(), periodic_update),
+    {ok, {Timer, StatsPid}}.
+
+handle_call(_Req, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast({update, Metric}, {_Timer, Pid} = State) ->
+    Val = antidote_stat:get_value(Pid, Metric),
+    exometer:update(Metric, Val),
+    {noreply, State};
+
+handle_cast(_Req, State) ->
+    {noreply, State}.
+
+handle_info(periodic_update, {OldTimer, Pid}) ->
+    erlang:cancel_timer(OldTimer),
+    update(Pid, [staleness]),
+    Timer = erlang:send_after(?INTERVAL, self(), periodic_update),    
+    {noreply, {Timer, Pid}}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+update(Pid, Metric) ->
+    Val = antidote_stats:get_value(Pid, Metric),
+    exometer:update(Metric, Val).
+
+init_metrics() ->   
+    Metrics = antidote_stats:stats(),
+    lists:foreach(fun(Metric) ->
+                          exometer:new(Metric, histogram, [{time_span, timer:seconds(60)}])
+                  end, Metrics).
+    
