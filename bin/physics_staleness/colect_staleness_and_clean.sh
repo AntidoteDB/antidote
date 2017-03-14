1a062cb17154e1ec6afeb693a5d305726e23da6a
Alejandro Z. Tomsic <aletomsic@gmail.com>
2017-03-14 15:37:23 +0100

    added script to tar all results in g5k tests

--- bin/physics_staleness/colect_staleness_and_clean.sh
+++ bin/physics_staleness/colect_staleness_and_clean.sh
@@ -4,7 +4,7 @@ tarAll () {
   local own_node_name="${HOSTNAME::-12}" # remove the .grid5000.fr part of the name
     local datafolder="~/antidote/_build/default/rel/antidote/data/"
     pushd "${datafolder}" > /dev/null 2>&1
-    local result_f_name="${own_node_name}-${CONFIG_FILE}.tar"
+    local result_f_name="node-${own_node_name}-StalenessResults.tar"
     tar czf /root/"${result_f_name}" "${datafolder}"
     popd > /dev/null 2>&1
 }
