7f43f1a525994ccd8a06fd04d7b9d8a713cb8313
Alejandro Z. Tomsic <aletomsic@gmail.com>
2017-03-16 18:08:35 +0100

    made join dc call a single time start processes

--- bin/launch-nodes.sh
+++ bin/launch-nodes.sh
@@ -5,3 +5,4 @@ for i in `seq 1 $1`;
 do
   PLATFORM_DATA_DIR="data/${i}" RING_STATE_DIR="data/ring/${i}" HANDOFF_PORT=8${i}99 PB_PORT=8${i}87 PUBSUB_PORT=8${i}86 LOGREADER_PORT=8${i}85 INSTANCE_NAME=antidote${i} PB_IP=127.0.0.1 COOKIE=antidote _build/default/rel/antidote${i}/bin/env foreground &
 done
+wait
--- bin/stop-nodes.sh
+++ bin/stop-nodes.sh
@@ -4,5 +4,5 @@ export RELX_REPLACE_OS_VARS=true
 for i in `seq 1 $1`;
 do
    _build/default/rel/antidote${i}/bin/antidote${i} stop
-  sleep 1
 done
+wait
