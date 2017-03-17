414c5351018478fd8e982bc10ee01fed9dc63037
Alejandro Z. Tomsic <aletomsic@gmail.com>
2017-03-17 11:50:03 +0100

    updated tar script

--- bin/physics_staleness/tar-staleness-results-g5k.sh
+++ bin/physics_staleness/tar-staleness-results-g5k.sh
@@ -4,11 +4,11 @@ tarAll () {
 #    local own_node_name="${HOSTNAME::-12}" # remove the .grid5000.fr part of the name
     local own_node_name="${HOSTNAME}" # remove the .grid5000.fr part of the name
     local datafolder="_build/default/rel/antidote/data/"
-    local result_f_name="node-${own_node_name}-StalenessResults.tar"
+    local result_f_name="node-${own_node_name}$1-StalenessResults.tar"
     cd
     cd antidote
     tar -czvf "../${result_f_name}" "${datafolder}"
 }
 
 
-tarAll
+tarAll "$@"
