0774d6fb9c9e7d4afca38ea10c78b70eb9c818ed
Alejandro Z. Tomsic <aletomsic@gmail.com>
2017-03-19 12:47:16 +0100

    updated tar script

--- bin/physics_staleness/tar-staleness-results-g5k.sh
+++ bin/physics_staleness/tar-staleness-results-g5k.sh
@@ -7,7 +7,7 @@ tarAll () {
     local result_f_name="node-${own_node_name}$1-StalenessResults.tar"
     cd
     cd antidote
-    tar -czvf "../${result_f_name}" "${datafolder}"
+    tar czf "../${result_f_name} --directory=${datafolder}"
 }
 
 
