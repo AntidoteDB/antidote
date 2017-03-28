1453ef38ab7c7f698c67930c852c7d8f108c9c5a
Alejandro Z. Tomsic <aletomsic@gmail.com>
2017-03-28 16:26:55 +0200

    updated staleness directory

--- bin/physics_staleness/tar-staleness-results-g5k.sh
+++ bin/physics_staleness/tar-staleness-results-g5k.sh
@@ -3,7 +3,7 @@
 tarAll () {
 #    local own_node_name="${HOSTNAME::-12}" # remove the .grid5000.fr part of the name
     local own_node_name="${HOSTNAME}" # remove the .grid5000.fr part of the name
-    local datafolder="_build/default/rel/antidote/data/Stale-*"
+    local datafolder="_build/default/rel/antidote/Stale-*"
     local result_f_name="node-${own_node_name}$1-StalenessResults.tar"
     cd
     cd antidote
