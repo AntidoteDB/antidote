849a218690511dc43fc40e4377abf7ad3a107694
Alejandro Z. Tomsic <aletomsic@gmail.com>
2017-03-28 16:30:34 +0200

    added taring logs

--- bin/physics_staleness/tar-staleness-results-g5k.sh
+++ bin/physics_staleness/tar-staleness-results-g5k.sh
@@ -4,10 +4,11 @@ tarAll () {
 #    local own_node_name="${HOSTNAME::-12}" # remove the .grid5000.fr part of the name
     local own_node_name="${HOSTNAME}" # remove the .grid5000.fr part of the name
     local datafolder="_build/default/rel/antidote/Stale-*"
+    local logfolder="_build/default/rel/antidote/Log-*"
     local result_f_name="node-${own_node_name}$1-StalenessResults.tar"
     cd
     cd antidote
-    command="tar czf ../${result_f_name} ${datafolder}"
+    command="tar czf ../${result_f_name} ${datafolder} ${logfolder}"
     echo "[NODE] $own_node_name : running "$command
     $command
 }
