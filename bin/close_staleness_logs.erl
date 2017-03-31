5744c47469a90e5220def5aa9203e88eb6907982
Alejandro Z. Tomsic <aletomsic@gmail.com>
2017-03-31 12:09:22 +0200

    added close staleness

--- bin/close_staleness_logs.erl
+++ bin/close_staleness_logs.erl
@@ -1,6 +1,6 @@
 #!/usr/bin/env escript
 %% -*- erlang -*-
-%%! -smp enable -name sync_staleness@127.0.0.1 -cookie antidote
+%%! -smp enable -name close_staleness@127.0.0.1 -cookie antidote
 -mode(compile).
 
 %% This should be called like (e.g.): truncate_staleness_logs.erl 'antidote1@1.2.3.4' 'antidote2@5.6.7.8'
@@ -20,13 +20,13 @@ main(NodesListString) ->
         bad_input_format ->
             usage();
         _->
-%%            lists:foreach(fun (Node) -> rpc:call(Node, materializer_vnode, sync_all_staleness_logs, []) end, Nodes),
-            rpc:call(hd(Nodes), materializer_vnode, sync_all_staleness_logs, []),
-            io:format("~nSuccesfully sent  rpc:call(Node, materializer_vnode, sync_all_staleness_logs, []) end, Nodes) to node: ~w~n", [hd(Nodes)])
+%%            lists:foreach(fun (Node) -> rpc:call(Node, materializer_vnode, close_all_staleness_logs, []) end, Nodes),
+            rpc:call(hd(Nodes), materializer_vnode, close_all_staleness_logs, []),
+            io:format("~nSuccesfully sent  rpc:call(Node, materializer_vnode, close_all_staleness_logs, []) end, Nodes) to node: ~w~n", [hd(Nodes)])
     end.
 
 usage() ->
-    io:format("This should be called like (e.g.): sync_all_staleness_logs.erl 'antidote1@1.2.3.4' 'antidote2@5.6.7.8'"),
+    io:format("This should be called like (e.g.): close_all_staleness_logs.erl 'antidote1@1.2.3.4' 'antidote2@5.6.7.8'"),
     halt(1).
 
 -include_lib("eunit/include/eunit.hrl").
