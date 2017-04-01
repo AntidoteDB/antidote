c3c251770ea92b214a9cd0b495ec170df077392a
Alejandro Z. Tomsic <aletomsic@gmail.com>
2017-04-01 16:11:38 +0200

    changed bench logs folder

--- bin/physics_staleness/tar-staleness-results-g5k.sh
+++ bin/physics_staleness/tar-staleness-results-g5k.sh
@@ -3,9 +3,9 @@
 tarAll () {
 #    local own_node_name="${HOSTNAME::-12}" # remove the .grid5000.fr part of the name
     local own_node_name="${HOSTNAME}" # remove the .grid5000.fr part of the name
-    local datafolder="_build/default/rel/antidote/benchLogs/Staleness"
-    local logfolder="_build/default/rel/antidote/benchLogs/Log"
-    local result_f_name="node-${own_node_name}$1-StalenessResults.tar"
+    local datafolder="benchLogs/Staleness"
+    local logfolder="/benchLogs/Log"
+    local result_f_name="node-${own_node_name}-$1-$2-StalenessResults.tar"
     cd
     cd antidote
     command="tar czf ../${result_f_name} ${datafolder} ${logfolder}"
