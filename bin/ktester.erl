4af88d14d179f3cbe8ef52c096dd6452c00af113
lplit <mrudek@gmail.com>
2017-08-01 03:20:16 +0200

    Example for comprehension

--- bin/ktester.erl
+++ bin/ktester.erl
@@ -8,23 +8,35 @@
 -define(dbug, io:format("dbug ~p ~p...~n", [?FUNCTION_NAME, ?LINE])).
 -define(tab, tabtab).
 -define(node, 'antidote@127.0.0.1').
-
+-define(kstab, 2).
 
 main(_A) ->
-    {ok, connected} = antidote_connect(?node),
-    DC1 = {'antidote1@127.0.0.1', {1501, 537303, 598423}},
-    DC2 = {'antidote2@127.0.0.1', {1390, 186897, 698677}},
-    DC3 = {'antidote3@127.0.0.1', {1490, 186159, 768617}},
-    DC4 = {'antidote4@127.0.0.1', {1590, 184597, 573977}},
+    antidote_connect(?node),
+    DC1_1 = {'antidote1@127.0.0.1', {1501, 537303, 598423}},
+    DC1_2 = {'antidote2@127.0.0.1', {1390, 186897, 698677}},
+    DC1_3 = {'antidote3@127.0.0.1', {1490, 186159, 768617}},
+    DC1_4 = {'antidote4@127.0.0.1', {1590, 184597, 573977}},
     A = rpc:call(?node, vectorclock, new, []),
-    AA = rpc:call(?node, vectorclock, set_clock_of_dc, [DC1, 1, A]),
-    AAA = rpc:call(?node, vectorclock, set_clock_of_dc, [DC2, 4, AA]),
-    AAAA = rpc:call(?node, vectorclock, set_clock_of_dc, [DC3, 0, AAA]),
-    VC = rpc:call(?node, vectorclock, set_clock_of_dc, [DC4, 3, AAAA]),
+    AA = rpc:call(?node, vectorclock, set_clock_of_dc, [DC1_1, 1, A]),
+    AAA = rpc:call(?node, vectorclock, set_clock_of_dc, [DC1_2, 4, AA]),
+    AAAA = rpc:call(?node, vectorclock, set_clock_of_dc, [DC1_3, 0, AAA]),
+    VC = rpc:call(?node, vectorclock, set_clock_of_dc, [DC1_4, 3, AAAA]),
     ets:new(?tab, [set, named_table]),
-    ets:insert(?tab, {DC1, VC}),
-    %io:format("~p ~n", ets:tab2list(?tab)),
-    io:format("~p ~n", [ets:lookup(?tab, DC1)]),
+    ets:insert(?tab, {DC1_1, VC}),
+    io:format("Lookup DC1_1~n~p~n", [ets:lookup(?tab, DC1_1)]),
+    %% Example output
+    %%    [{{'antidote1@127.0.0.1',{1501,537303,598423}},
+    %%        {dict,4,16,16,8,80,48,
+    %%            {[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]},
+    %%            {{[],[],[],
+    %%                [[{'antidote4@127.0.0.1',{1590,184597,573977}}|3]],
+    %%                [[{'antidote2@127.0.0.1',{1390,186897,698677}}|4]],
+    %%                [[{'antidote3@127.0.0.1',{1490,186159,768617}}|0]],
+    %%                [],[],
+    %%                [[{'antidote1@127.0.0.1',{1501,537303,598423}}|1]],
+    %%                [],[],[],[],[],[],[]}}}}]
+    io:format("Whole tab~n~p~nCalculating K stable for K=~p~n", [ets:tab2list(?tab), ?kstab]),
+
     ok.
 
 %% Connects
