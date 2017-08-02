9623d491e83f7198a266fccfefe1b2d02e80bca7
lplit <mrudek@gmail.com>
2017-08-02 23:45:51 +0200

    take input

--- bin/launch-nodes.sh
+++ bin/launch-nodes.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 export RELX_REPLACE_OS_VARS=true
-
-for i in `seq 1 10`;
+for i in `seq 1 $1`;
 do
   PLATFORM_DATA_DIR="data/${i}" RING_STATE_DIR="data/ring/${i}" HANDOFF_PORT=8${i}99 PB_PORT=8${i}87 PUBSUB_PORT=8${i}86 LOGREADER_PORT=8${i}85 METRICS_PORT=300${i} NODE_NAME=antidote-${i} _build/default/rel/antidote/bin/antidote foreground &
   sleep 1
