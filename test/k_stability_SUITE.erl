7f0ffd73726a866bb5619bb8b2405d9e13bc5196
lplit <mrudek@gmail.com>
2017-08-06 20:07:07 +0200

    Add k_stability_SUITE test suite

---
+++ test/k_stability_SUITE.erl
@@ -0,0 +1,76 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2014 SyncFree Consortium.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+-module(k_stability_SUITE).
+-compile({parse_transform, lager_transform}).
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("kernel/include/inet.hrl").
+
+%% common_test callbacks
+-export([
+    init_per_suite/1,
+    end_per_suite/1,
+    init_per_testcase/2,
+    end_per_testcase/2,
+    all/0]).
+
+% Tests
+-export([
+    kvector_test/1,
+    versionMatrix_test/1
+]).
+
+init_per_suite(Config) ->
+    test_utils:at_init_testsuite(),
+    Clusters = test_utils:set_up_clusters_common(Config),
+    Nodes = lists:flatten(Clusters),
+
+    %Ensure that the clocksi protocol is used
+    test_utils:pmap(fun(Node) ->
+        rpc:call(Node, application, set_env,
+            [antidote, txn_prot, clocksi]) end, Nodes),
+
+    %Check that indeed clocksi is running
+    {ok, clocksi} = rpc:call(hd(hd(Clusters)), application, get_env, [antidote, txn_prot]),
+
+    [{clusters, Clusters} | Config].
+
+end_per_suite(Config) ->
+    Config.
+
+init_per_testcase(_Case, Config) ->
+    Config.
+
+end_per_testcase(_, _) ->
+    ok.
+
+all() -> [kvector_test, versionMatrix_test].
+
+kvector_test(Config) ->
+    Clusters = proplists:get_value(clusters, Config),
+
+    pass.
+
+versionMatrix_test(Config) ->
+    Clusters = proplists:get_value(clusters, Config),
+
+    pass.
