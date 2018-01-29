1173b0244682e6b02c6617a98eaed36f399d5e87
aletomsic <aletomsic@gmail.com>
2018-01-29 10:25:28 +0100

    improved error handling vnode

--- bin/physics_staleness/tar-staleness-results-g5k.sh
+++ bin/physics_staleness/tar-staleness-results-g5k.sh
@@ -3,8 +3,8 @@
 tarAll () {
 #    local own_node_name="${HOSTNAME::-12}" # remove the .grid5000.fr part of the name
     local own_node_name="${HOSTNAME}" # remove the .grid5000.fr part of the name
-    local datafolder="~/antidote/benchLogs/Staleness"
-    local logfolder="~/antidote/benchLogs/Log"
+    local datafolder="/tmp/antidote/benchLogs/Staleness"
+    local logfolder="/tmp/antidote/benchLogs/Log"
     local result_f_name="node-${own_node_name}-$1-$2-StalenessResults.tar"
     cd
     cd antidote
