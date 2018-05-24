3bc64d97054b5827e6747cb7282270aa5949ebd5
Albert Schimpf <github@schimpf.link>
2018-05-24 12:28:17 +0200

    Simplified and documented release test script

--- test/release_test.sh
+++ test/release_test.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-# This builds a release, starts it and tries to do simple transaction
-
+# This builds a release, starts it and tries to do simple transaction; exits immediately upon error
 set -e
 
+# cd to root project directory
 SCRIPTDIR=`dirname $0`
-pushd "$SCRIPTDIR/.." > /dev/null
-ROOT=`pwd -P`
-popd > /dev/null
-
-
-cd $ROOT
+cd "$SCRIPTDIR/.."
 
 # Start Antidote
 ./_build/default/rel/antidote/bin/env start
+
+# Wait for ports to be available for tcp connections
+sleep 1
 
 # Execute test transaction
 ./test/release_test.escript
