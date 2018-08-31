ca0c8c6f673796bda61a1f165fddb6c1047531f1
Albert Schimpf <github@schimpf.link>
2018-08-31 16:51:29 +0200

    Moven utils to own folder

--- tools.mk
+++ tools.mk
@@ -1,17 +1,22 @@
 REBAR ?= $(shell pwd)/rebar3
 
+compile-utils: compile
+	for filename in "test/utils/*.erl" ; do \
+		erlc -pa _build/default/lib/lager/ebin/ -o test/utils $$filename ; \
+	done
+
 test:
 	mkdir -p logs
 	${REBAR} eunit skip_deps=true
 	${REBAR} cover
 
-systests: rel
+systests: compile-utils rel
 	rm -f test/*.beam
 	mkdir -p logs
 ifdef SUITE
-	ct_run -pa ./_build/default/lib/*/ebin -logdir logs -suite test/${SUITE} -cover test/antidote.coverspec
+	ct_run -pa ./_build/default/lib/*/ebin test/utils/ -logdir logs -suite test/${SUITE} -cover test/antidote.coverspec
 else
-	ct_run -pa ./_build/default/lib/*/ebin -logdir logs -dir test -cover test/antidote.coverspec
+	ct_run -pa ./_build/default/lib/*/ebin test/utils/ -logdir logs -dir test -cover test/antidote.coverspec
 endif
 
 docs:
