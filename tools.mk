1b7c2fe41a8af6295dd108a9c40430d47a6c352e
Peter Zeller <p_zeller@cs.uni-kl.de>
2018-09-06 10:41:33 +0200

    updated riak_pb dependency

--- tools.mk
+++ tools.mk
@@ -1,13 +1,13 @@
 REBAR ?= $(shell pwd)/rebar3
 
 test: compile
- 	${REBAR} eunit skip_deps=true
+	${REBAR} eunit skip_deps=true
 
 docs:
- 	${REBAR} doc skip_deps=true
+	${REBAR} doc skip_deps=true
 
 xref:	compile
- 	${REBAR} xref skip_deps=true
+	${REBAR} xref skip_deps=true
 
 dialyzer:	compile
 	${REBAR} dialyzer
