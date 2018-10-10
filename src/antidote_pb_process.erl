a304f6c44d212ac5df863bbf883a37030396998a
bieniusa <bieniusa@cs.uni-kl.de>
2018-10-10 16:48:26 +0200

    Further adaptations to the new codec patterns

--- src/antidote_pb_process.erl
+++ src/antidote_pb_process.erl
@@ -28,22 +28,27 @@
 
 -export([process/1]).
 
+-spec decode_clock(binary()) -> snapshot_time() | ignore.
+decode_clock(Clock) ->
+    case Clock of
+        undefined -> ignore;
+        _ -> binary_to_term(Clock)
+    end.
+
+
 -spec process(antidote_pb_codec:request()) -> antidote_pb_codec:response().
 process({start_transaction, {Clock, Properties}}) ->
-    Response = antidote:start_transaction(Clock, Properties),
+    Response = antidote:start_transaction(decode_clock(Clock), Properties),
     case Response of
-        {ok, TxId} -> antidote_pb_codec:encode(start_transaction_response,
-                                             {ok, TxId});
-        {error, Reason} -> antidote_pb_codec:encode(start_transaction_response,
-                                             {error, Reason})
+        {ok, TxId} -> {start_transaction_response, {ok, TxId}};
+        {error, Reason} -> {start_transaction_response, {error, Reason}}
     end;
 
 process({abort_transaction, TxId}) ->
     Response = antidote:abort_transaction(TxId),
     case Response of
-        ok -> antidote_pb_codec:encode(operation_response, ok);
-        {error, Reason} -> antidote_pb_codec:encode(operation_response,
-                                             {error, Reason})
+        ok -> {operation_response, ok};
+        {error, Reason} -> {operation_response, {error, Reason}}
             %% TODO: client initiated abort is not implemented yet
             %% Add the following only after it is implemented to avoid dialyzer errors
             %% ok -> antidote_pb_codec:encode(operation_response, ok),
@@ -52,76 +57,67 @@ process({abort_transaction, TxId}) ->
 process({commit_transaction, TxId}) ->
     Response = antidote:commit_transaction(TxId),
     case Response of
-        {error, Reason} -> antidote_pb_codec:encode(commit_response,
-                                             {error, Reason});
-        {ok, CommitTime} -> antidote_pb_codec:encode(commit_response, {ok, CommitTime})
+        {error, Reason} -> {commit_response, {error, Reason}};
+        {ok, CommitTime} -> {commit_response, {ok, CommitTime}}
     end;
 
 process({update_objects, {Updates, TxId}}) ->
     Response = antidote:update_objects(Updates, TxId),
     case Response of
-        {error, Reason} -> antidote_pb_codec:encode(operation_response, {error, Reason});
-        ok -> antidote_pb_codec:encode(operation_response, ok)
+        {error, Reason} -> {operation_response, {error, Reason}};
+        ok -> {operation_response, ok}
     end;
 
 process({static_update_objects, {Clock, Properties, Updates}}) ->
-    Response = antidote:update_objects(Clock, Properties, Updates),
+    Response = antidote:update_objects(decode_clock(Clock), Properties, Updates),
     case Response of
-        {error, Reason} ->
-            antidote_pb_codec:encode(commit_response,
-                                             {error, Reason});
-        {ok, CommitTime} ->
-            antidote_pb_codec:encode(commit_response, {ok, CommitTime})
+        {error, Reason} -> {commit_response, {error, Reason}};
+        {ok, CommitTime} -> {commit_response, {ok, CommitTime}}
     end;
 
 process({read_objects, {Objects, TxId}}) ->
     Response = antidote:read_objects(Objects, TxId),
     case Response of
-        {error, Reason} -> antidote_pb_codec:encode(read_objects_response,
-                                             {error, Reason});
-        {ok, Results} -> antidote_pb_codec:encode(read_objects_response,
-                                             {ok, lists:zip(Objects, Results)})
+        {error, Reason} -> {read_objects_response, {error, Reason}};
+        {ok, Results} -> {read_objects_response, {ok, lists:zip(Objects, Results)}}
     end;
 
 
 process({static_read_objects, {Clock, Properties, Objects}}) ->
-    Response = antidote:read_objects(Clock, Properties, Objects),
+    Response = antidote:read_objects(decode_clock(Clock), Properties, Objects),
     case Response of
-        {error, Reason} ->
-            antidote_pb_codec:encode(commit_response,
-                                             {error, Reason});
-        {ok, Results, CommitTime} ->
-            antidote_pb_codec:encode(static_read_objects_response,
-                                             {ok, lists:zip(Objects, Results), CommitTime})
+        {error, Reason} ->{commit_response, {error, Reason}};
+        {ok, Results, CommitTime} -> {static_read_objects_response, {ok, lists:zip(Objects, Results), CommitTime}}
     end;
 
 process({create_dc, NodeNames}) ->
     try
-      ok = antidote_dc_manager:create_dc(NodeNames), antidote_pb_codec:encode(operation_response, ok)
+      ok = antidote_dc_manager:create_dc(NodeNames),
+      {operation_response, ok}
     catch
      Error:Reason -> %% Some error, return unsuccess. TODO: correct error response
        lager:info("Create DC Failed ~p : ~p", [Error, Reason]),
-       antidote_pb_codec:encode(operation_response, {error, create_dc_failed})
+       {operation_response, {error, create_dc_failed}}
     end;
 
 process({get_connection_descriptor}) ->
     try
        {ok, Descriptor} = antidote_dc_manager:get_connection_descriptor(),
-       antidote_pb_codec:encode(get_connection_descriptor_resp, {ok, term_to_binary(Descriptor)})
+       {get_connection_descriptor_resp, {ok, term_to_binary(Descriptor)}}
     catch
       Error:Reason -> %% Some error, return unsuccess. TODO: correct error response
         lager:info("Get Conection Descriptor ~p : ~p", [Error, Reason]),
-        antidote_pb_codec:encode(get_connection_descriptor_resp, {error, no_clue})
+        {get_connection_descriptor_resp, {error, no_clue}}
     end;
 
 process({connect_to_dcs, Descriptors}) ->
     try
        ok = antidote_dc_manager:subscribe_updates_from(Descriptors),
-       antidote_pb_codec:encode(operation_response, ok)
+       {operation_response, ok}
     catch
       Error:Reason -> %% Some error, return unsuccess. TODO: correct error response
         lager:info("Connect to DCs Failed ~p : ~p", [Error, Reason]),
-        antidote_pb_codec:encode(operation_response, {error, connect_to_dcs_failed})
+        {operation_response, {error, connect_to_dcs_failed}}
     end;
 
 process(Message) ->
