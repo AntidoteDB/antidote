e267241decc47650ca0d2584428998548cb3758c
deepthidevaki <deepthidevaki@gmail.com>
2018-10-11 10:25:04 +0200

    typo license

--- src/antidote_app.erl
+++ src/antidote_app.erl
@@ -17,10 +17,10 @@
 %% Unless required by applicable law or agreed to in writing,
 %% software distributed under the License is distributed on an
 %% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-%% KIND, either express or implied.  See the License for the
+%% KIND, either expressed or implied.  See the License for the
 %% specific language governing permissions and limitations
 %% under the License.
-%% 
+%%
 %% List of the contributors to the development of Antidote: see AUTHORS file.
 %% Description and complete License: see LICENSE file.
 %% -------------------------------------------------------------------
