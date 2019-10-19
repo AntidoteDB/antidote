e408c58682a850a079ec53814a711ddfa4038541
Albert Schimpf <38429047+albsch@users.noreply.github.com>
2019-10-19 09:22:00 +0200

    Migrated meta data from riak_metadata to antidote_metadata (#394)

--- src/antidote_hooks.erl
+++ src/antidote_hooks.erl
@@ -93,7 +93,7 @@ register_pre_hook(Bucket, Module, Function) ->
 register_hook(Prefix, Bucket, Module, Function) ->
     case erlang:function_exported(Module, Function, 1) of
         true ->
-            riak_core_metadata:put(Prefix, Bucket, {Module, Function}),
+            stable_meta_data_server:broadcast_meta_data({Prefix, Bucket}, {Module, Function}),
             ok;
         false ->
             {error, function_not_exported}
@@ -101,16 +101,22 @@ register_hook(Prefix, Bucket, Module, Function) ->
 
 -spec unregister_hook(pre_commit | post_commit, bucket()) -> ok.
 unregister_hook(pre_commit, Bucket) ->
-    riak_core_metadata:delete(?PREFIX_PRE, Bucket);
-
+    stable_meta_data_server:broadcast_meta_data({?PREFIX_PRE, Bucket}, undefined);
 unregister_hook(post_commit, Bucket) ->
-    riak_core_metadata:delete(?PREFIX_POST, Bucket).
+    stable_meta_data_server:broadcast_meta_data({?PREFIX_POST, Bucket}, undefined).
 
 get_hooks(pre_commit, Bucket) ->
-    riak_core_metadata:get(?PREFIX_PRE, Bucket);
-
+    R = stable_meta_data_server:read_meta_data({?PREFIX_PRE, Bucket}),
+    case R of
+        {ok, Hooks} -> Hooks;
+        error -> undefined
+    end;
 get_hooks(post_commit, Bucket) ->
-    riak_core_metadata:get(?PREFIX_POST, Bucket).
+    R = stable_meta_data_server:read_meta_data({?PREFIX_POST, Bucket}),
+    case R of
+        {ok, Hooks} -> Hooks;
+        error -> undefined
+    end.
 
 -spec execute_pre_commit_hook(term(), type(), op_param()) ->
         {term(), type(), op_param()} | {error, reason()}.
