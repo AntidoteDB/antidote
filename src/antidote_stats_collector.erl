5ff0b4736199e6e6ec0b326b3edb6055c7960731
Albert Schimpf <git@schimpf.link>
2019-11-12 19:32:59 +0100

    added warning count

--- src/antidote_stats_collector.erl
+++ src/antidote_stats_collector.erl
@@ -54,6 +54,10 @@ handle_cast(log_error, State) ->
     prometheus_counter:inc(antidote_error_count),
     {noreply, State};
 
+handle_cast(log_warning, State) ->
+    prometheus_counter:inc(antidote_warning_count),
+    {noreply, State};
+
 handle_cast(open_transaction, State) ->
     prometheus_gauge:inc(antidote_open_transactions),
     prometheus_counter:inc(antidote_transaction, [open]),
@@ -201,7 +205,8 @@ init_metrics() ->
     prometheus_gauge:new([{name, antidote_process_message_queue_length}, {help, "Message queue length of processes"}, {labels, [process]}]),
 
     prometheus_counter:new([{name, antidote_log_size}, {help, "The size of a single log file in bytes"}, {labels, [log]}]),
-    prometheus_counter:new([{name, antidote_error_count}, {help, "The number of error encountered during operation"}]),
+    prometheus_counter:new([{name, antidote_error_count}, {help, "The number of errors encountered during operation"}]),
+    prometheus_counter:new([{name, antidote_warning_count}, {help, "The number of warnings encountered during operation"}]),
     prometheus_histogram:new([{name, antidote_staleness}, {help, "The staleness of the stable snapshot"}, {buckets, [1, 10, 100, 1000, 10000]}]),
     prometheus_gauge:new([{name, antidote_open_transactions}, {help, "Number of open transactions"}]),
     prometheus_counter:new([{name, antidote_transaction}, {help, "Number of transactions by type"}, {labels, [type]}]),
