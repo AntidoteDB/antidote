a8bbde0117407416c12694c670b71dd86341da2b
Albert Schimpf <git@schimpf.link>
2019-11-19 10:21:50 +0100

    Ranch V2 api changes

--- src/antidote_pb_sup.erl
+++ src/antidote_pb_sup.erl
@@ -50,5 +50,14 @@ pb_listener() ->
   NumberOfAcceptors = application:get_env(ranch, pb_pool_size, 100),
   Port = application:get_env(ranch, pb_port, 8087),
   MaxConnections = application:get_env(ranch, pb_max_connections, 1024),
-  RanchOptions = [{port, Port}, {max_connections, MaxConnections}],
-  ranch:child_spec(antidote_pb_process, NumberOfAcceptors, ranch_tcp, RanchOptions, antidote_pb_protocol, []).
+
+  ListenerSpec = ranch:child_spec({?MODULE, antidote_pb_process},
+    ranch_tcp, #{
+      num_acceptors => NumberOfAcceptors,
+      max_connections => MaxConnections,
+      socket_opts => [{port, Port}]
+    }, antidote_pb_protocol, []
+  ),
+
+  ListenerSpec.
+
