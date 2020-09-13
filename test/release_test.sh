858b795dfb3e76ce6fbc309a83b120c84179bf33
Albert Schimpf <git@schimpf.link>
2020-09-13 10:01:10 +0200

    Fixed reltest for newest rebar3 API

--- test/release_test.sh
+++ test/release_test.sh
@@ -5,13 +5,14 @@ set -e
 # set test node name
 export NODE_NAME=antidote@127.0.0.1
 export ROOT_DIR_PREFIX=antidote@127.0.0.1/
+export COOKIE=testcookie
 
 # cd to root project directory
 SCRIPTDIR=`dirname $0`
 cd "$SCRIPTDIR/.."
 
 # Start Antidote
-./_build/default/rel/antidote/bin/antidote start
+./_build/default/rel/antidote/bin/antidote daemon
 
 # Execute test transaction
 ./test/release_test.escript
