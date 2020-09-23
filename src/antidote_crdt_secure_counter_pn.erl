3559fb4ec7c5466987ef4f89031ac73b59e0e745
João Marques <jb.marques@campus.fct.unl.pt>
2020-09-23 21:25:26 +0100

    Require N square parameter in all operations of the secure PN-Counter

--- src/antidote_crdt_secure_counter_pn.erl
+++ src/antidote_crdt_secure_counter_pn.erl
@@ -50,55 +50,57 @@
 
 -type freshness() :: fresh | spoiled.
 -type delta() :: integer().
--type nsquare() :: integer().
--type state() :: {freshness(), integer(), nsquare()}.
--type op() :: {increment, delta()} | {increment, {delta(), nsquare()}}.
--type effect() :: delta() | {delta(), nsquare()}.
+-type nsquare() :: pos_integer().
+-type state() :: {freshness(), integer()}.
+-type op() :: {increment, {delta(), nsquare()}}.
+-type effect() :: {delta(), nsquare()}.
 
-%% @doc Create a new, empty 'antidote_crdt_secure_counter_pn'
+%% @doc Create a new, empty 'antidote_crdt_secure_counter_pn'.
+%%
+%% The first element of the state tuple indicates whether the counter
+%% is newly created (no increments done yet) or not. The second element
+%% represents the encrypted (as specified by the Paillier scheme) total
+%% value of the counter.
 -spec new() -> state().
 new() ->
-    {fresh, 0, 0}.
+    {fresh, 0}.
 
-%% @doc The single, total value of a secure pn-counter.
+%% @doc Returns the encrypted (as specified by the Paillier scheme) total
+%% value of a secure pn-counter.
 -spec value(state()) -> integer().
-value({_, Value, _}) when is_integer(Value) ->
+value({_, Value}) when is_integer(Value) ->
     Value.
 
 %% @doc Generate a downstream operation.
-%% The first parameter is either a tuple of the form `{increment, integer()}'
-%% or a tuple of the form `{increment, {integer(), integer()}}'. The first
-%% integer represents the delta value by which the counter will be incremented,
-%% while the second integer is the n squared value used by the Paillier
-%% cryptosystem. The second parameter is the secure pn-counter, this parameter
-%% is not actually used.
--spec downstream(op(), state()) -> {ok, effect()}.
-downstream({increment, {Delta, NSquare}}, _SecurePNCounter)
-    when is_integer(Delta) and is_integer(Delta)
-->
-    {ok, {Delta, NSquare}};
-downstream({increment, Delta}, _SecurePNCounter) when is_integer(Delta) ->
-    {ok, Delta}.
-
-%% @doc Updates a given secure pn-counter, incrementing it by a given `Delta'.
-%% By incrementing we mean performing the homomorphic addition of the counter
-%% value with the given delta. As described by the Paillier cryptosystem, the
-%% homomorphic addition of plaintexts translates to the product of two
-%% ciphertexts modulo n squared.
 %%
-%% A value for `NSquare` may also be provided, if it isn't, the one in the
-%% counter state will be used.
+%% The first parameter is a tuple of the form `{increment, {integer(), pos_integer()}}`.
+%% Where the first integer represents the encrypted delta value by which the counter
+%% will be incremented. The second integer represents the N squared value calculated
+%% during the key generation phase of the Paillier cryptosystem.
+%%
+%% The value of N is part of the user's public key, it is ok for the server to know this
+%% value. Invalid `NSquare` values (less than or equal to zero) are rejected, and a
+%% downstream effect is not generated.
+%%
+%% The second parameter is the secure pn-counter, this parameter is not actually used.
+-spec downstream(op(), state()) -> {ok, effect()}.
+downstream({increment, {Delta, NSquare}}, _SecurePNCounter) when
+    is_integer(Delta) and is_integer(NSquare) and (NSquare > 0)
+->
+    {ok, {Delta, NSquare}}.
+
+%% @doc Updates a given secure pn-counter, incrementing it by a given
+%% encrypted `Delta'. By incrementing we mean performing the homomorphic
+%% addition of the counter value with the given delta. As described by
+%% the Paillier cryptosystem, the homomorphic addition of two plaintexts
+%% translates to the product of two ciphertexts modulo N squared.
 %%
 %% Returns the updated secure pn-counter.
 -spec update(effect(), state()) -> {ok, state()}.
-update({Delta, NSquare}, {fresh, _, _}) ->
-    {ok, {spoiled, Delta, NSquare}};
-update({Delta, NSquare}, {spoiled, Value, _}) ->
-    {ok, {spoiled, (Value * Delta) rem NSquare, NSquare}};
-update(Delta, {fresh, _, NSquare}) ->
-    {ok, {spoiled, Delta, NSquare}};
-update(Delta, {spoiled, Value, NSquare}) ->
-    {ok, {spoiled, (Value * Delta) rem NSquare, NSquare}}.
+update({Delta, _NSquare}, {fresh, _Value}) ->
+    {ok, {spoiled, Delta}};
+update({Delta, NSquare}, {spoiled, Value}) ->
+    {ok, {spoiled, (Value * Delta) rem NSquare}}.
 
 %% @doc Compare if two secure counters are equal.
 -spec equal(state(), state()) -> boolean().
@@ -116,9 +118,12 @@ from_binary(Bin) ->
 %% @doc The following function verifies that a given operation is supported by
 %% this particular CRDT.
 -spec is_operation(term()) -> boolean().
-is_operation({increment, {Delta, NSquare}}) when is_integer(Delta) and is_integer(NSquare) -> true;
-is_operation({increment, Delta}) when is_integer(Delta) -> true;
-is_operation(_) -> false.
+is_operation({increment, {Delta, NSquare}}) when
+    is_integer(Delta) and is_integer(NSquare) and (NSquare > 0)
+->
+    true;
+is_operation(_) ->
+    false.
 
 %% @doc Returns true if ?MODULE:downstream/2 needs the state of crdt
 %% to generate downstream effect.
@@ -136,33 +141,46 @@ prepare_and_effect(Op, PNCounter) ->
     update(Downstream, PNCounter).
 
 new_test() ->
-    ?assertEqual({fresh, 0, 0}, new()).
+    ?assertEqual({fresh, 0}, new()).
 
 value_test() ->
-    ?assertEqual(3, value({fresh, 3, 2})),
-    ?assertEqual(4, value({spoiled, 4, 2})).
+    ?assertEqual(0, value({fresh, 0})),
+    ?assertEqual(4, value({spoiled, 4})).
 
-%% @doc Tests wether the secure pn-counter is correctly updated.
+%% @doc Tests whether the secure pn-counter is correctly updated.
 update_test() ->
     Counter = new(),
-    % Fresh counter without a given n squared.
-    {ok, Counter1} = prepare_and_effect({increment, 1}, Counter),
-    ?assertEqual({spoiled, 1, 0}, Counter1),
-    % Fresh counter with a given n squared.
-    {ok, Counter2} = prepare_and_effect({increment, {1, 1}}, Counter),
-    ?assertEqual({spoiled, 1, 1}, Counter2),
-    % Spoiled counter.
-    {ok, Counter3} = prepare_and_effect({increment, 2}, Counter2),
-    ?assertEqual({spoiled, 0, 1}, Counter3),
-    {ok, Counter4} = prepare_and_effect({increment, {2, 3}}, Counter2),
-    ?assertEqual({spoiled, 2, 3}, Counter4).
+    % Fresh counter becomes spoiled.
+    {ok, Counter1} = prepare_and_effect({increment, {2, 1}}, Counter),
+    ?assertEqual({spoiled, 2}, Counter1),
+    % Spoiled counter stays spoiled and correctly updates value.
+    {ok, Counter2} = prepare_and_effect({increment, {3, 36}}, Counter1),
+    ?assertEqual({spoiled, 6}, Counter2).
+
+%% @doc Updates should only accept positive N square values.
+reject_invalid_nsquare_test() ->
+    Counter = new(),
+    Operation1 = {increment, {1,  0}},
+    Operation2 = {increment, {1, -1}},
+    ?assertNot(is_operation(Operation1)),
+    ?assertNot(is_operation(Operation2)),
+    ?assertError(function_clause, downstream(Operation1, Counter)),
+    ?assertError(function_clause, downstream(Operation2, Counter)).
 
 equal_test() ->
-    Counter1 = {fresh, 4, 0},
-    Counter2 = {fresh, 2, 0},
-    Counter3 = {fresh, 2, 0},
+    Counter1 = {fresh, 4},
+    Counter2 = {fresh, 2},
+    Counter3 = {fresh, 2},
     ?assertNot(equal(Counter1, Counter2)),
-    ?assert(equal(Counter2, Counter3)).
+    ?assert(equal(Counter2, Counter3)),
+
+    Counter4 = {spoiled, 5},
+    Counter5 = {spoiled, 3},
+    Counter6 = {spoiled, 3},
+    ?assertNot(equal(Counter4, Counter5)),
+    ?assert(equal(Counter5, Counter6)),
+
+    ?assertNot(equal(Counter1, Counter4)).
 
 binary_test() ->
     Counter1 = {spoiled, 4, 5},
