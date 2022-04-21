6cc94b8658f3f257b90e8259c97ea4477ba2504a
v0idpwn <v0idpwn@gmail.com>
2022-04-21 13:31:45 +0300

    Add antidotec_pb and ecto as dependencies

--- mix.exs
+++ mix.exs
@@ -22,8 +22,8 @@ defmodule Vax.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto, "~> 3.7.0"},
+      {:antidotec_pb, "~> 0.2.9"}
     ]
   end
 
